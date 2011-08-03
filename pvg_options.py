a6022f91c740ea30066f51ecfb99e45b5aabf53a
Giorgio Gilestro <giorgio@gilest.ro>
2011-08-03 15:14:24 +0100

    Made more pretty

--- pvg_options.py
+++ pvg_options.py
@@ -29,41 +29,45 @@ class pvg_OptionsPanel(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent, wx.ID_ANY)
         self.parent = parent
-        self.sp = wx.ScrolledWindow(self, wx.ID_ANY, size=(-1, 400))
         
-        self.sp.SetScrollbars(1, 1, 1, 1)
-        self.sp.SetScrollRate(10, 10)
-
+        ########
+        mainSizer = wx.BoxSizer(wx.HORIZONTAL)
+        optionsList = [key for key in options.defaultOptions]
+        self.tb = wx.ListBox(self, wx.ID_ANY, choices=optionsList, style=wx.LB_SINGLE)
+        self.Bind(wx.EVT_LISTBOX, self.onSelect, self.tb)
+        #########
+        
+        #########
+        self.sp = wx.Panel(self, wx.ID_ANY)
         sz1 = wx.BoxSizer(wx.VERTICAL)
-    
         
         sz2 = wx.BoxSizer(wx.VERTICAL)
         titleFont = wx.Font(10, wx.SWISS, wx.NORMAL, wx.BOLD)
         items = []
         self.input = []
 
+        all_keys = [k for k in options.defaultOptions]
+        key = all_keys[0]
+        default_value = str(options.defaultOptions[key][0])
+        text = options.defaultOptions[key][1]
+        all_values = [str(options.GetOption(key)) for key in all_keys]
+        self.values = dict( [ (key, value) for (key, value) in zip(all_keys, all_values)] )                           
+
+        self.Title = wx.StaticText(self.sp, wx.ID_ANY, '%s' % key)
+        self.Title.SetFont(titleFont)
+        self.Description = wx.StaticText(self.sp, -1,  '\n%s.\nDefault value = %s\n' % (text, default_value) )
+        self.Input = wx.TextCtrl(self.sp, -1, all_values[0] )
+        self.Bind(wx.EVT_TEXT, self.onChangeValues, self.Input)
+
+        sz2.Add(self.Title, 0, wx.LEFT|wx.ALL, 2)
+        sz2.Add(self.Description, 0, wx.LEFT|wx.ALL, 2)
+        sz2.Add(self.Input, 0, wx.LEFT|wx.ALL, 2)
+        sz2.Add ( (wx.StaticLine(self.sp)), 0, wx.EXPAND|wx.TOP|wx.BOTTOM, 5 )
         
-        for key in options.defaultOptions:
-            default_value = str(options.defaultOptions[key][0])
-            text = options.defaultOptions[key][1]
-            act_value = str( options.GetOption(key) )
-      
-
-            items.append ( wx.StaticText(self.sp, -1, '\n%s' % key) )
-            items[-1].SetFont(titleFont)
-            sz2.Add(items[-1], 0, wx.LEFT|wx.ALL, 2)
-
-            items.append (  wx.StaticText(self.sp, -1, '\n%s.\nDefault value = %s' % (text, default_value) ) )
-            sz2.Add(items[-1], 0, wx.LEFT|wx.ALL, 2)
-
-            self.input.append ( wx.TextCtrl(self.sp, -1, act_value ))
-            sz2.Add(self.input[-1], 0, wx.LEFT|wx.ALL, 2)
-
-            sz2.Add ( (wx.StaticLine(self.sp)), 0, wx.EXPAND|wx.TOP|wx.BOTTOM, 5 )
-            
         self.sp.SetSizer(sz2)
+        #########
 
-        ##
+        #########
         btnSz = wx.BoxSizer(wx.HORIZONTAL)
         saveBtn = wx.Button(self, wx.ID_SAVE)
         clearBtn = wx.Button(self, wx.ID_CANCEL)
@@ -71,13 +75,38 @@ class pvg_OptionsPanel(wx.Panel):
         self.Bind(wx.EVT_BUTTON, self.onSave, saveBtn)
         btnSz.Add (saveBtn, 0, wx.ALL, 5) 
         btnSz.Add (clearBtn, 0, wx.ALL, 5) 
+        #########
 
-        ##
         sz1.Add (self.sp, 1, wx.EXPAND|wx.ALL, 10)
         sz1.Add (btnSz, 0, wx.EXPAND, 0)
         
-        self.SetSizer(sz1)
+        mainSizer.Add(self.tb, 0, wx.EXPAND|wx.ALL,1)
+        mainSizer.Add(sz1, 0, wx.EXPAND|wx.ALL,1)
+        
+        self.SetSizer(mainSizer)
 
+    def onChangeValues(self, event):
+        """
+        """
+        key = self.Title.GetLabel()
+        self.values[key] = event.GetString()
+
+    def onSelect(self, event):
+        """
+        """
+        key = event.GetString()
+        self.updatePanel(key)
+
+    def updatePanel(self, key):
+        """
+        """
+        default_value = str(options.defaultOptions[key][0])
+        text = options.defaultOptions[key][1]
+        act_value = self.values[key]
+        
+        self.Title.SetLabel('%s' % key)
+        self.Description.SetLabel('\n%s.\nDefault value = %s' % (text, default_value))
+        self.Input.SetValue(act_value)        
         
     def onCancel(self, event):
         """
@@ -87,10 +116,10 @@ class pvg_OptionsPanel(wx.Panel):
     def onSave(self, event):
         """
         """
-        values = [v.GetValue() for v in self.input]
         keys = [key for key in options.defaultOptions]
         
-        for k, v in zip ( keys, values):
+        for k in keys:
+            v = self.values[k]
             v = v.replace('(',''); v = v.replace(')','')
             options.SetValue('Options', k, v)
         
