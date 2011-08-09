892c7f5e0b103998d433b98dbbe3bc110fc7f3c7
Giorgio Gilestro <giorgio@gilest.ro>
2011-08-09 16:24:01 +0100

    Added support for movie making and mask hiding

--- pvg_standalone.py
+++ pvg_standalone.py
@@ -4,32 +4,22 @@ import pysolovideo as pv
 from pvg_common import previewPanel, pvg_config
 
 class CvMovieFrame(wx.Frame):
-    def __init__(self, parent, source, resolution, track, track_type, mask_file, outputFile ):
+    def __init__(self, parent, source, resolution, track, track_type, mask_file, outputFile, showROIs, record ):
         wx.Frame.__init__(self, parent)
-        self.displayPanel = previewPanel(self, size=resolution)
+        self.displayPanel = previewPanel(self, size=resolution, keymode=True)
         self.SetSize(resolution)
-        self.Show()
 
         self.displayPanel.setMonitor(source, resolution)
         self.displayPanel.mon.setTracking(track, track_type, mask_file, outputFile)
-        self.displayPanel.Play()
-
-
         
-
-def start(source, resolution, track, track_type, mask_file, outputFile):
-    """
-    start the wx window
-    """
-    
-    app = wx.App()
-    f = CvMovieFrame(None, source, resolution, track, track_type, mask_file, outputFile )
-    app.MainLoop()    
-
+        if record:
+            self.displayPanel.mon.saveMovie('video_output.avi', fps=14, startOnKey=True)
+        
+        self.displayPanel.Play(showROIs=showROIs)
+        self.Show()
 
 
 if __name__=="__main__":
-
 
     parser = optparse.OptionParser(usage='%prog [options] [argument]>', version='%prog version 0.1')
     parser.add_option('-c', '--config', dest='configfile', metavar="CONFIGFILE", help="Config mode | Use specified CONFIGFILE")
@@ -38,6 +28,8 @@ if __name__=="__main__":
     parser.add_option('-k', '--mask', dest='mask_file', metavar="MASKFILE", help="File mode | Specify a maskfile to be used with file.")
     parser.add_option('-t', '--tracktype', dest='track_type', metavar="TT", help="File mode | Specify track type: 0, distance; 1, trikinetics; 2, coordinates")
     parser.add_option('-o', '--output', dest='outputFile', metavar="OUTFILE", help="All modes | Specify an output file where to store tracking results. A Mask must be loaded")
+    parser.add_option('--showmask', action="store_true", default=False, dest='showROIs', help="Show the area limiting the ROIs")
+    parser.add_option('--record', action="store_true", default=False, dest='record', help="Record the resulting video as avi file")
     
     (options, args) = parser.parse_args()
 
@@ -48,7 +40,6 @@ if __name__=="__main__":
         _,source,track,mask_file,track_type = opts.GetMonitor(mon)
         resolution = opts.GetOption('FullSize')
         outputFile = options.outputFile or ''
-        start( source, resolution, track, track_type, mask_file, outputFile )
         
     elif options.source:
         
@@ -59,9 +50,13 @@ if __name__=="__main__":
         track_type = options.track_type
         track = mask_file and track_type
         outputFile = options.outputFile
-        start( source, resolution, track, track_type, mask_file, outputFile )
 
-    
     else:
         parser.print_help()
 
+
+    if (options.configfile and options.monitor) or options.source:
+
+        app = wx.App()
+        f = CvMovieFrame(None, source, resolution, track, track_type, mask_file, outputFile, options.showROIs, options.record )
+        app.MainLoop()    
