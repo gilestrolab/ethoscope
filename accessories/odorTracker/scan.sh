07d3feb0a8ecd43683d07af6e812b2799f77a055
Giorgio Gilestro <giorgio@gilest.ro>
2012-05-08 16:52:50 +0100

    Added Zenity interface to script

--- accessories/odorTracker/scan.sh
+++ accessories/odorTracker/scan.sh
@@ -1,22 +1,78 @@
 #!/bin/bash
 
-SRC=$1
-PVGROOT="/home/gg/Dropbox/myCode/pySolo-Video/"
+PYT='python2'
+PVGROOT="`pwd`/../../"
+OTROOT="`pwd`/"
+MOVIE_FILETYPE="*.ogv*"
 
-PVG_S=$PVGROOT"pvg_standalone.py -t2 --trackonly -i"
-FILETYPE_S="*.ogv*"
+ACTIONS=$(zenity --list --checklist --title "What do you want to do." --text "Specify actions." --column "" --column "Choices" True "Draw the Mask" True "Write Coordinates" True "Draw Graphs")
 
-PVG_OT=$PVGROOT"accessories/odorTracker/odorTracker.py --distribution --path -i"
-FILETYPE_OT="*.txt"
+MAKEMASK=0
+COORDS=0
+GRAPHS=0
 
-for file in `find ${SRC} -name "$FILETYPE_S" -type f`
-do
-    echo ${file}
-    python2 ${PVG_S} ${file}
-done
 
-for file in `find ${SRC} -name "$FILETYPE_OT" -type f`
-do
-    echo ${file}
-    python2 ${PVG_OT} ${file}
-done
+if [[ $ACTIONS =~ .*Mask.* ]]
+then
+  MAKEMASK=1
+fi
+
+if [[ $ACTIONS =~ .*Coordinates.* ]]
+then
+  COORDS=1
+fi
+
+if [[ $ACTIONS =~ .*Graphs.* ]]
+then
+  GRAPHS=1
+fi
+
+
+SRC=$(zenity  --file-selection --title="Select Directory that contains the files you want to process" --directory --filename=.)
+
+if [ -z $SRC ]; then
+
+    echo Aborted
+   
+else
+
+    echo Processing
+    
+    cd $SRC
+
+    #MAKE MASK
+    if [ $MAKEMASK = 1 ]; then
+        PVG_MM=$PVGROOT"pvg_standalone.py -t2 --showmask -i"
+
+        for file in `find ${SRC} -name "$MOVIE_FILETYPE" -type f`
+        do
+            echo 'Now calculating coordinates for ' ${file}
+            $PYT ${PVG_MM} ${file}
+        done
+    fi
+
+    #CALCULATE COORDINATES
+    if [ $COORDS = 1 ]; then
+        PVG_S=$PVGROOT"pvg_standalone.py -t2 --trackonly -i"
+
+        for file in `find ${SRC} -name "$MOVIE_FILETYPE" -type f`
+        do
+            echo 'Now calculating coordinates for ' ${file}
+            $PYT ${PVG_S} ${file}
+        done
+    fi
+
+
+    #DRAW GRAPHS
+    if [ $GRAPHS = 1 ]; then
+        PVG_OT=$OTROOT"odorTracker.py --distribution --path -i"
+        FILETYPE_COORD="*.txt"
+
+        for file in `find ${SRC} -name "$FILETYPE_COORD" -type f`
+        do
+            echo 'Now processing figures for' ${file}
+            $PYT ${PVG_OT} ${file}
+        done
+    fi
+
+fi
