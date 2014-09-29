d52d6a41bc5ea286df404e151d5dc95938ae434b
Quentin Geissmann <qgeissmann@gmail.com>
2014-09-29 19:48:14 +0100

    prepare wrapers for the cameras.py sub-module

--- src/pysolovideo/tracking/cameras.py
+++ src/pysolovideo/tracking/cameras.py
@@ -6,14 +6,54 @@ import cv2
 
 
 class BaseCamera(object):
-    pass
 
-class PhysicalCamera(BaseCamera):
-    pass
+    capture = None
+    resolution = None
 
-class VirtualCamera(BaseCamera):
+    def __init__(self):
+
+        raise NotImplementedError
+
+    def __del__(self):
+        self.capture.close()
+
+    def __iter__(self):
+        while True:
+            if self.is_last_frame() or not self.is_opened():
+                break
+
+            yield self._next_image()
+
+
+    def is_opened(self):
+        raise NotImplementedError
+
+    @property
+    def resolution(self):
+        return self.resolution
+
+    @property
+    def width(self):
+        return self.resolution[0]
+
+    @property
+    def height(self):
+        return self.resolution[1]
+
+    def is_last_frame(self):
+        raise NotImplementedError
+    def _next_image(self):
+        raise NotImplementedError
+
+class BasePhysicalCamera(BaseCamera):
+    def is_last_frame(self):
+        return False
+
+
+class BaseVirtualCamera(BaseCamera):
     pass
 
 class USBCamera(PhysicalCamera):
     pass
+
 
