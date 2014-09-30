7c1d71bfc541759274e6c87f44a63f996fd7caa2
Quentin Geissmann <qgeissmann@gmail.com>
2014-09-30 17:13:24 +0100

    prototype the monitoring

--- prototypes/iter_video.py
+++ prototypes/iter_video.py
@@ -2,15 +2,20 @@ __author__ = 'quentin'
 
 
 from pysolovideo.tracking.cameras import MovieVirtualCamera
-
+import cv2
 
 
 cam = MovieVirtualCamera("/home/quentin/Desktop/drosoAdult_short.avi")
 
-import cv2
+
 for t,frame in cam:
     cv2.imshow("Test", frame)
     cv2.waitKey(1)
 
+cam.restart()
+
+for t,frame in cam:
+    cv2.imshow("Test", frame)
+    cv2.waitKey(1)
 
 
--- prototypes/monitoring_test.py
+++ prototypes/monitoring_test.py
@@ -1,1 +1,12 @@
 __author__ = 'quentin'
+
+
+from pysolovideo.tracking.monitor import Monitor
+from pysolovideo.tracking.cameras import MovieVirtualCamera
+from pysolovideo.tracking.trackers import DummyTracker
+
+cam = MovieVirtualCamera("/home/quentin/Desktop/drosoAdult_short.avi")
+
+monit = Monitor(cam, DummyTracker)
+monit.run()
+
