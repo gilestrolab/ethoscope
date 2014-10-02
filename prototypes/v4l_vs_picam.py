4a40c468a615b9958dac545999533a8b7a319784
Quentin Geissmann <qgeissmann@gmail.com>
2014-10-02 19:57:51 +0100

    play with image resolution

--- prototypes/v4l_vs_picam.py
+++ prototypes/v4l_vs_picam.py
@@ -6,8 +6,12 @@ import numpy as np
 import time
 
 capture = cv2.VideoCapture(0)
+capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,640*2)
+capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480*2)
+
 cv2.waitKey(2000)
 _,im = capture.read()
+
 
 NFRAMES = 1000
 print "ok, frame shape=", im.shape
