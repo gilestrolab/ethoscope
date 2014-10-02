acf0a6a22dafc0dea9358e2850b3c9cc6b609a6c
Quentin Geissmann <qgeissmann@gmail.com>
2014-10-02 19:54:28 +0100

    try without np.copy

--- prototypes/v4l_vs_picam.py
+++ prototypes/v4l_vs_picam.py
@@ -15,7 +15,7 @@ print "ok, frame shape=", im.shape
 t0 = time.time()
 for _ in range(NFRAMES):
     _, im = capture.read()
-    im = np.copy(im)
+    #im = np.copy(im)
     assert(len(im.shape) == 3)
 t1= time.time()
 
