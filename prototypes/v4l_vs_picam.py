70872300de0e183ba701c00a8ff3b7e784e96965
Quentin Geissmann <qgeissmann@gmail.com>
2014-10-02 20:02:49 +0100

    play with image resolution2

--- prototypes/v4l_vs_picam.py
+++ prototypes/v4l_vs_picam.py
@@ -6,8 +6,8 @@ import numpy as np
 import time
 
 capture = cv2.VideoCapture(0)
-capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,640*2)
-capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480*2)
+capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,640*1.5)
+capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480*1.5)
 
 cv2.waitKey(2000)
 _,im = capture.read()
