d469c4e4838b316c97f27c80f6de57660e6d61a7
Quentin Geissmann <qgeissmann@gmail.com>
2014-10-02 16:53:05 +0100

    benchmarkin a simple capture and subsequent array copy

---
+++ prototypes/v4l_vs_picam.py
@@ -0,0 +1,22 @@
+__author__ = 'quentin'
+
+
+import cv2
+import numpy as np
+import time
+
+capture = cv2.VideoCapture(0)
+im = capture.read()
+
+NFRAMES = 1000
+print "ok"
+
+t0 = time.time()
+for _ in range(NFRAMES):
+    _, im = capture.read()
+    im = np.copy(im)
+    assert(len(im.shape) == 3)
+t1= time.time()
+
+print t0,t1
+print t1-t0 / float(NFRAMES)
