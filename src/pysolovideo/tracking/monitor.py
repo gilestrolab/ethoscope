bug fixed: positions between tracking units were a shared variable. Implementation of a generic position display. Posiotions are now relative to the size of the ROI major axis

src/pysolovideo/tracking/monitor.py
@@ -4,7 +4,7 @@ import roi_builders as rbs
 from tracking_unit import TrackingUnit
 import logging
 import cv2
-
+import numpy as np
 class Monitor(object):
 
     def __init__(self, camera, tracker_class, roi_builder = None, interactors=None):
@@ -29,12 +29,34 @@ class Monitor(object):
 
 
 
+    def _draw_on_frame(self,t, frame):
+        for track_u in self._unit_trackers:
+
+            xy, wh, a = track_u.get_last_position(absolute=True)
+
+            if np.isnan(xy):
+                continue
+
+            x = int(np.round(np.real(xy)))
+            y = int(np.round(np.imag(xy)))
+
+            w = int(np.round(np.real(wh)))
+            h = int(np.round(np.imag(wh)))
+
+            a = int(np.round(np.real(a)))
+
+            cv2.ellipse(frame,((x,y), (w,h), a),(0,255,0),2)
+            cv2.drawContours(frame,[track_u.roi.polygon],-1, (255,0,0), 2)
+        cv2.imshow("el", frame)
+        cv2.waitKey(1)
+
     def run(self):
-         for t, frame in self._camera:
-             for track_u in self._unit_trackers:
+        for t, frame in self._camera:
+            for track_u in self._unit_trackers:
                 track_u(t, frame)
-                track_u.interactor()
-             #cv2.waitKey(30)
+                # track_u.interactor()
+            self._draw_on_frame(t,frame)
+
 
 
 

src/pysolovideo/tracking/roi_builders.py
@@ -13,13 +13,17 @@ class ROI(object):
 
         # TODO if we do not need polygon, we can drop it
         self._polygon = np.array(polygon)
+        if len(self._polygon.shape) == 2:
+            self._polygon = self._polygon.reshape((self._polygon.shape[0],1,self._polygon.shape[1]))
+
+
         self._value = value
-        #print self._polygon
+
 
         x,y,w,h = cv2.boundingRect(self._polygon)
 
         self._mask = np.zeros((h,w), np.uint8)
-        cv2.drawContours(self._mask, [polygon], 0, 255,-1,offset=(-x,-y))
+        cv2.drawContours(self._mask, [self._polygon], 0, 255,-1,offset=(-x,-y))
 
         self._rectangle = x,y,w,h
 
@@ -31,6 +35,22 @@ class ROI(object):
 
         return
 
+    @property
+    def offset(self):
+        x,y,w,h = self._rectangle
+        return x + 1j*y
+
+    @property
+    def polygon(self):
+
+        return self._polygon
+
+
+    @property
+    def longest_axis(self):
+        x,y,w,h = self._rectangle
+        return float(max(w, h))
+
 
     @property
     def value(self):
@@ -39,6 +59,7 @@ class ROI(object):
     def __call__(self,img):
         x,y,w,h = self._rectangle
         out = img[y : y + h, x : x +w]
+
 
         assert(out.shape[0:2] == self._mask.shape)
 
@@ -69,13 +90,12 @@ class DefaultROIBuilder(BaseROIBuilder):
 
     def _rois_from_img(self,img):
         h, w = img.shape[0],img.shape[1]
-
         return[
             ROI([
                 (   0,        0       ),
-                (   0,        w -1    ),
-                (   h - 1,    w - 1   ),
-                (   h - 1,    0       )]
+                (   0,        h -1    ),
+                (   w - 1,    h - 1   ),
+                (   w - 1,    0       )]
         )]
 
 

src/pysolovideo/tracking/trackers.py
@@ -1,30 +1,38 @@
 __author__ = 'quentin'
 
 import numpy as np
-import cv
 import cv2
-from sklearn.cluster import MeanShift, estimate_bandwidth
 import itertools
+import roi_builders
+
 class NoPositionError(Exception):
     pass
 
 class BaseTracker(object):
     # a list of complex number representing x(real) and y(imaginary) coordinatesT
-    _positions = []
-    _time_stamps = []
-    _data=None
 
-    def __init__(self, data=None):
+
+    def __init__(self, roi,data=None):
+        self._positions = []
+        self._time_stamps = []
+
         self._data = data
+        self._roi = roi
 
-    def __call__(self, t, img, mask):
+
+    def __call__(self, t, img):
+        sub_img, mask = self._roi(img)
+
         try:
-            point = self._find_position(img,mask)
+            point = self._find_position(sub_img,mask)
+            point[0:2] = point[0:2] / self._roi.longest_axis
+
         except NoPositionError:
             if len(self._positions) == 0:
                 point = np.array([np.NaN] * 3)
             else:
                 point = self._positions[-1]
+
 
 
         self._positions.append(point)
@@ -56,87 +64,11 @@ class DummyTracker(BaseTracker):
             return point
         return self._positions[-1] + point
 #
-#
-class AdaptiveMOGTracker(BaseTracker):
-#     def __init__(self, data=None):
-#         self._mog = cv2.BackgroundSubtractorMOG(1000,2, 0.9, 1.0)
-#
-#         self._max_learning_rate = 1e-2
-#         self._learning_rate = self._max_learning_rate
-#         self._min_learning_rate = 1e-7
-#         self._increment = 1.2
-#         super(AdaptiveMOGTracker, self).__init__(data)
-#
-#     def _find_position(self, img, mask):
-#         # TODO preallocated buffers
-#         # TODO  slice me with mask
-#         #  TODO try exepect slice me with mask
-#
-#
-#
-#         tmp = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-#
-#         tmp = cv2.GaussianBlur(tmp,(3,3), 1.5)
-#
-#         tmp = self._mog.apply(tmp, None, self._learning_rate)
-#
-#
-#         if len(self._positions) > 0:
-#             pos = self._positions[-1]
-#         else:
-#             pos= np.NaN
-#
-#
-#
-#
-#         # TODO if  a large proportion of the image is True, we should abort here.
-#
-#         yx = np.where(tmp)
-#
-#         if len(yx[0]) < 10:
-#             self._learning_rate *= self._increment
-#         else:
-#             yx = np.column_stack((yx[1],yx[0]))
-#
-#             # FIXME magic number here
-#             ms = MeanShift(bandwidth=10, bin_seeding=True)
-#             ms.fit(yx)
-#             labels = ms.labels_
-#             labels_unique = np.unique(labels)
-#             n_clusters_ = len(labels_unique)
-#
-#
-#             if n_clusters_ > 1:
-#                 self._learning_rate *= self._increment
-#             else:
-#                 hull = cv2.convexHull(yx)
-#
-#                 self._learning_rate /= self._increment
-#                 moments = cv2.moments(hull)
-#                 pos = moments['m10']/moments['m00'] + 1j * moments['m01']/moments['m00']
-#
-#
-#         if self._learning_rate > self._max_learning_rate:
-#             self._learning_rate = self._max_learning_rate
-#
-#         if self._learning_rate < self._min_learning_rate:
-#             self._learning_rate = 0
-#
-#         try:
-#             cv2.drawContours(img,[hull],0, (255,0,0),2)
-#         except:
-#             pass
-#         # cv2.imshow(str(self),img)
-#
-#
-#         return pos, np.NaN
-#
-    pass
-
 
 
 class AdaptiveBGModel(BaseTracker):
-    def __init__(self, data=None):
+
+    def __init__(self, roi, data=None):
         self._max_learning_rate = 1e-1
         self._learning_rate = self._max_learning_rate
         self._min_learning_rate = 1e-3
@@ -146,7 +78,8 @@ class AdaptiveBGModel(BaseTracker):
         self._fg_features = None
         self._bg_sd = None
 
-        super(AdaptiveBGModel, self).__init__(data)
+        super(AdaptiveBGModel, self).__init__(roi, data)
+
 
 
     def _update_fg_blob_model(self, img, contour, lr = 1e-2):
@@ -201,7 +134,7 @@ class AdaptiveBGModel(BaseTracker):
             self._bg_mean = img
             self._bg_sd = img
 
-        learning_mat = np.ones_like(img) * self._learning_rate
+        learning_mat = np.ones_like(img,dtype = np.float32) * np.float32(self._learning_rate)
         if fgmask is not None:
             cv2.dilate(fgmask,None,fgmask)
             cv2.dilate(fgmask,None,fgmask)
@@ -261,7 +194,6 @@ class AdaptiveBGModel(BaseTracker):
 
     def _find_position(self, img, mask):
         # TODO preallocated buffers
-        # TODO  slice me with mask
         #  TODO try exepect slice me with mask
 
         # minor preprocessing
@@ -270,22 +202,25 @@ class AdaptiveBGModel(BaseTracker):
 
         if len(self._positions) == 0:
             self._update_bg_model(grey)
-            print "??????"
             raise NoPositionError
 
         # fixme this should NOT be needed !
-        if self._bg_mean is None:
+        elif self._bg_mean is None:
             self._update_bg_model(grey)
             print "fixme"
             raise NoPositionError
 
-        fg = np.abs(grey - self._bg_mean).astype(np.uint8)
+        # fixme use preallocated buffers next line
+        fg = grey - self._bg_mean
+        np.abs(fg, fg)
+        fg = fg.astype(np.uint8)
+
         cv2.dilate(fg,None,fg)
         cv2.erode(fg,None,fg)
         #cv2.imshow("f", fg*5)
 
-        #todo make thi objective
-        _, fg = cv2.threshold(fg,13,255,cv2.THRESH_BINARY)
+        #todo make this objective
+        cv2.threshold(fg,13,255,cv2.THRESH_BINARY, dst=fg)
 
         if mask is not None:
             #todo test me
@@ -319,15 +254,15 @@ class AdaptiveBGModel(BaseTracker):
 
         self._update_fg_blob_model(grey, hull)
 
-        fg_mask = np.zeros_like(grey)
+        fg.fill(0)
 
         (x,y) ,(w,h), angle  = cv2.minAreaRect(hull)
 
-        cv2.drawContours(fg_mask ,[hull],0, (255,0,0),-1)
+        cv2.drawContours(fg ,[hull],0, (255,0,0),-1)
 
-        cv2.imshow(str(self), fg_mask )
-        cv2.waitKey(1)
+        # cv2.imshow(str(self), fg )
+        # cv2.waitKey(1)
 
-        self._update_bg_model(grey, fg_mask)
+        self._update_bg_model(grey, fg)
 
-        return [x + 1j * y, w + 1j * h, angle]
+        return np.array([x + 1j * y, w + 1j * h, angle])

src/pysolovideo/tracking/tracking_unit.py
@@ -3,11 +3,11 @@ __author__ = 'quentin'
 
 
 import interactors
-
+import numpy as np
 
 class TrackingUnit(object):
     def __init__(self, tracking_algo_class, roi, interactor=None):
-        self._tracker = tracking_algo_class()
+        self._tracker = tracking_algo_class(roi)
         self._roi = roi
 
         if interactor is not None:
@@ -24,14 +24,27 @@ class TrackingUnit(object):
     @property
     def roi(self):
         return self._roi
+    def get_last_position(self,absolute=False):
+        last_position = self._tracker.positions[-1]
+
+        if np.isnan(last_position[0]):
+            return last_position
+        if not absolute:
+            return last_position
+
+        out = np.copy(last_position)
+
+        out[0:2] = out[0:2] * self._roi.longest_axis
+
+        out[0] = out[0] + self._roi.offset
+        return out
+
+
 
     def __call__(self, t, img):
-        sub_img, mask = self._roi(img)
+        self._tracker(t,img)
 
-        position = self._tracker(t,sub_img, mask)
-        self._interactor()
-        return 1
-        # return position
+
 
 
 