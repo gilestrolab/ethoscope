0c4d25ecb7a1bfc6f2df9ea29eca6ca93e45c651
Quentin Geissmann <qgeissmann@gmail.com>
2014-10-23 16:59:31 +0100

    A globa

--- prototypes/rois_from_img_sd.py
+++ prototypes/rois_from_img_sd.py
@@ -6,6 +6,7 @@ from pysolovideo.tracking.cameras import MovieVirtualCamera
 from pysolovideo.tracking.monitor import Monitor
 from pysolovideo.tracking.trackers import AdaptiveBGModel
 from pysolovideo.tracking.interactors import SleepDepInteractor
+from pysolovideo.hardware_control.arduino_api import SleepDepriverInterface
 
 import cv2
 
@@ -20,9 +21,9 @@ rb = ImgMaskROIBuilder("/stk/pysolo_video_samples/maskOf_sleepdep_150min_night.p
 rois = rb(cam)
 
 
+sdi = SleepDepriverInterface()
 
-
-inters = [SleepDepInteractor(i) for i in range(13)]
+inters = [SleepDepInteractor(i, sdi) for i in range(13)]
 
 
 
