198613100d336cfce486fca4c6641029e87a3164
Quentin Geissmann <qgeissmann@gmail.com>
2014-12-10 17:03:27 +0000

    include explicit option to write a debuging/repport annotated video file as part of Monitor

--- src/pysolovideo/tracking/monitor.py
+++ src/pysolovideo/tracking/monitor.py
@@ -8,11 +8,14 @@ import cv2
 
 import pandas as pd
 
+# todo make capable of recording result video in arbitrary location
+# todo write every row /chunk of data in stdout/file (use updatable pd.Dataframes?)
 
 class Monitor(object):
 
     def __init__(self, camera, tracker_class, rois = None, interactors=None, out_file=None,
                 draw_results=False, draw_every_n=1,
+                video_out = None,
                 max_duration=None):
 
         self._camera = camera
@@ -20,7 +23,7 @@ class Monitor(object):
         self._draw_results = draw_results
         self.draw_every_n = draw_every_n
         self._max_duration = max_duration
-
+        self._video_out = video_out
         if rois is None:
             rois = rbs.DefaultROIBuilder(camera)()
 
@@ -37,25 +40,24 @@ class Monitor(object):
 
 
 
-    def _draw_on_frame(self, frame, wait = 1):
+    def _draw_on_frame(self, frame):
 
-        frame = frame.copy()
+        frame_cp = frame.copy()
         for track_u in self._unit_trackers:
+
             pos = track_u.get_last_position(absolute=True)
-            cv2.putText(frame, str(track_u.roi.idx + 1),track_u.roi.offset, cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.75, (255,255,0))
+            cv2.putText(frame_cp, str(track_u.roi.idx + 1), track_u.roi.offset, cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.75, (255,255,0))
             if pos is None:
                 continue
+
             # if "interact" in pos.keys() and pos["interact"]:
             #     colour = (0, 255, 255)
             # else:
             colour = (0, 0, 255)
-            cv2.drawContours(frame,[track_u.roi.polygon],-1, colour, 1, cv2.CV_AA)
-            cv2.ellipse(frame,((pos["x"],pos["y"]), (pos["w"],pos["h"]), pos["phi"]),(255,0,0),1,cv2.CV_AA)
+            cv2.drawContours(frame_cp,[track_u.roi.polygon],-1, colour, 1, cv2.CV_AA)
+            cv2.ellipse(frame_cp,((pos["x"],pos["y"]), (pos["w"],pos["h"]), pos["phi"]),(255,0,0),1,cv2.CV_AA)
 
-
-        cv2.imshow("psv", frame)
-        cv2.waitKey(wait)
-        return frame
+        return frame_cp
 
     def _stop_monitor(self, out):
         if self._out_file is not None:
@@ -64,15 +66,15 @@ class Monitor(object):
 
     def run(self):
         out  = []
-        # vw = None
+        vw = None
         try:
             for i,(t, frame) in enumerate(self._camera):
                 if self._max_duration is not None and t > self._max_duration:
                     self._stop_monitor(out)
                     break
 
-                # if vw is None:
-                #     vw = cv2.VideoWriter("/home/quentin/Desktop/tracking_with_motion.avi", cv2.cv.CV_FOURCC(*'DIVX'), 50, (frame.shape[1], frame.shape[0]))
+                if self._video_out is not None and vw is None:
+                    vw = cv2.VideoWriter(self._video_out, cv2.cv.CV_FOURCC(*'DIVX'), 50, (frame.shape[1], frame.shape[0])) # fixme the 50 is arbitrary
 
                 for track_u in self._unit_trackers:
                     data_row = track_u(t, frame)
@@ -82,14 +84,18 @@ class Monitor(object):
                     if self._out_file is not None:
                         out.append(data_row)
 
-                if self._draw_results and i % self.draw_every_n == 0:
-                    tmp = self._draw_on_frame(frame)
 
-                        # vw.write(tmp)
+                if (self._draw_results and i % self.draw_every_n == 0) or not vw is None :
+                    tmp = self._draw_on_frame(frame)
+                    if (self._draw_results and i % self.draw_every_n == 0):
+                        cv2.imshow("psv", tmp)
+                        cv2.waitKey(1)
+                    if not vw is None:
+                        vw.write(tmp)
 
         except KeyboardInterrupt:
             pass
 
         self._stop_monitor(out)
-
-        #vw.release()
+        if not vw is None:
+            vw.release()
