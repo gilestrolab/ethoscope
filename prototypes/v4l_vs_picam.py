922709a131cb85a4691251e8240abac81bb42002
Quentin Geissmann <qgeissmann@gmail.com>
2014-12-16 16:02:56 +0000

    v4l2 interface seems broken on the pi. reprototyping simple wrapper

--- prototypes/v4l_vs_picam.py
+++ prototypes/v4l_vs_picam.py
@@ -6,8 +6,8 @@ import numpy as np
 import time
 
 capture = cv2.VideoCapture(0)
-capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,640*1.5)
-capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480*1.5)
+capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,640)
+capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
 
 cv2.waitKey(2000)
 _,im = capture.read()
@@ -19,9 +19,11 @@ print "ok, frame shape=", im.shape
 t0 = time.time()
 for _ in range(NFRAMES):
     _, im = capture.read()
+    cv2.imshow("frame", im)
+    cv2.waitKey(1)
+
     #im = np.copy(im)
     assert(len(im.shape) == 3)
 t1= time.time()
 
-print t0,t1
 print (t1-t0) / float(NFRAMES)
