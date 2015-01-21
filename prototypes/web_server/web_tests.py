ab5066e14c2d8ff2cc2b96166434c15be81cc013
Quentin Geissmann <qgeissmann@gmail.com>
2015-01-21 13:45:35 +0000

    prototype threading for webserver

--- prototypes/web_server/web_tests.py
+++ prototypes/web_server/web_tests.py
@@ -1,15 +1,17 @@
 __author__ = 'quentin'
 
 import time
+from threading import Thread, Lock
 
 class Dummy(object):
 
     def __init__(self):
         self._is_alive = True
+        self._iteration = 0
 
     @property
     def dummy_variable(self):
-        return "hello"
+        return self._iteration
 
     def stop(self):
         self._is_alive = False
@@ -20,11 +22,19 @@ class Dummy(object):
     def run(self):
         while self._is_alive:
             time.sleep(1)
+            self._iteration +=1
             pass
 
+
+def test_foo():
+    dummy.run()
 
 if __name__ == "__main__":
 
     dummy = Dummy()
-    dummy.run()
+    lock = Lock()
+
+
+
+    Thread(target=test_foo, args=(a,lock)).start()
 
