91687c58ceb4c1a57b5ee222a251568a8263f8e0
pepelisu <pepelisu@gmail.com>
2015-01-21 14:23:29 +0000

    protopype update and new ControlThread class

--- prototypes/web_server/web_tests.py
+++ prototypes/web_server/web_tests.py
@@ -1,40 +1,17 @@
 __author__ = 'quentin'
 
 import time
-from threading import Thread, Lock
-
-class Dummy(object):
-
-    def __init__(self):
-        self._is_alive = True
-        self._iteration = 0
-
-    @property
-    def dummy_variable(self):
-        return self._iteration
-
-    def stop(self):
-        self._is_alive = False
-
-    def __del__(self):
-        self.stop()
-
-    def run(self):
-        while self._is_alive:
-            time.sleep(1)
-            self._iteration +=1
-            pass
-
-
-def test_foo():
-    dummy.run()
+from pysolovideo.web_utils.control_thread import ControlThread
 
 if __name__ == "__main__":
 
-    dummy = Dummy()
-    lock = Lock()
-
-
-
-    Thread(target=test_foo, args=(a,lock)).start()
+    track = ControlThread()
+    track.start()
+    try:
+        while True:
+            time.sleep(2)
+            print (track.dummy_variable)
+    finally:
+        track.stop()
+        
 
