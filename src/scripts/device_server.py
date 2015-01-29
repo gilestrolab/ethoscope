80fd3b1629a4fb09b64c26077d504714bc62c746
Quentin Geissmann <qgeissmann@gmail.com>
2015-01-29 11:32:06 +0000

    stdout and gzip handled proprely now. need to check files are closed ok though

--- src/scripts/device_server.py
+++ src/scripts/device_server.py
@@ -31,8 +31,7 @@ def controls(id, action):
                     # set time, given in milliseconds from javascript, used in seconds for date
                     #set_time = call(['date', '-s', '@' + str(t)[:-3]])
                     #FIXME should not draw unless for debug
-                    control = ControlThread(machine_id, out_file=OUT_CSV_FILE, draw_results = True)
-
+                    control = ControlThread(machine_id, out_file=OUT_CSV_FILE, draw_results = True, max_duration=60*60)
 
                     control.start()
                     return {'status': 'Started'}
@@ -46,7 +45,7 @@ def controls(id, action):
                 try:
                     return control.format_psv_error(e)
                 except:
-                    return {type(e):str(e)}
+                    return {type(e).__name__:str(e)}
 
     else:
         return "Error on machine ID"
@@ -80,7 +79,9 @@ if __name__ == '__main__':
     #OUT_CSV_FILE = zipfile.ZipFile("/tmp/out.zip","w",zipfile.ZIP_DEFLATED)
     #OUT_CSV_FILE =
     import gzip
-    OUT_CSV_FILE = gzip.open("/tmp/out.csv.gz","wb")
+    import sys
+    OUT_CSV_FILE = gzip.open("/tmp/out.csv.gz","w")
+    # OUT_CSV_FILE = sys.stdout
 
     #create object
     control = None #ControlThread(machine_id)
