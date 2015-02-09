bffdb11ad4b9bc3452f1180f20908f2dbfd12a47
Quentin Geissmann <qgeissmann@gmail.com>
2015-02-09 18:13:53 +0000

    prototype of data analysis pipeline

---
+++ r_package/prototypes/activity.R
@@ -0,0 +1,40 @@
+library(risonno)
+FILE <- "~/Desktop/test2.db"
+
+
+
+out <- loadROIsFromFile(FILE, FUN=function(d)max(d$t))
+max_t <- max(unlist(out))
+roi_dfs <- loadROIsFromFile(FILE, 
+	FUN=interpolateROIData, start=0,
+	stop = max_t, fs=1)
+
+activity <- function(d){
+	comp = d$x + 1i*d$y
+	distance <- c(0, abs(diff(comp)))
+	d$activity <- distance
+	return(d)
+}
+
+
+roi_dfs <- lapply(roi_dfs, activity)
+
+pdf(w=16,h=9)
+lapply(names(roi_dfs), function(n) {
+	
+	d <- roi_dfs[[n]]
+	tt <- d$t/3600;
+	
+	y <- filter(d$activity, rep(1,601))
+	sparse_idx <- seq(0, length(tt), length.out=1e4)
+	tt <- tt[sparse_idx]
+	y <- y[sparse_idx]
+
+	plot(y ~ tt, type='l',ylim=c(0,50),
+	xlab="time(h)",
+	ylab="Activity (tube lenght walked in 10min)",
+	main=n)
+	abline(v = 40.25 + -10 :10 * 12, col="blue", lwd=3, lty=2)
+	return(NULL)
+	})
+dev.off()
