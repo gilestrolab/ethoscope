fd7e522ba42e73157baa14c9b251370757469ec6
Quentin Geissmann <qgeissmann@gmail.com>
2015-02-09 17:20:39 +0000

    update package stub: interpolation function, flexible data loading(on the go lapplys), and metadata loading

--- r_package/risonno/R/interpol.R
+++ r_package/risonno/R/interpol.R
@@ -1,55 +1,61 @@
 NULL
 
-#' Resample data from an area matrix.
+#' Resample ROI data to a regular multivariate time series.
 #' 
-#' This function returns position from reads at a regular time interval.
-#' This is perfomed through linear interpolation.
-#' @param m a numerical matrix corresponding to an area.
-#' @param h the desired resampling frequency in Hz.
-#' @param minRow an integer defining the minimal number of reads. If less than minRow reads are present in m, the function returns an empty matrix.
-#' @return A new area matrix.
-#' @note The new matrix will likely have a different number of rows than the source matrix. 
-#' The attributes of the source matrix are copied to the new matrix.
-#' @examples
-#' #TODO
+#' This function performs linear interpolation in order to obtain a regular time series from a possibly irregular one.
 #'
-#@seealso \code{\link{ubitMedianFilter}} to smooth data (before interpolation).
+#' @param d a dataframe containing a time column (\code{d$t}) and one or several columns for recorded variables.
+#' @param fs the desired resampling frequency, in Hz. By default, the median empirical sampling rate is used.
+#' @param min_n_point an integer defining the minimal number of reads. If fewer reads are present in \code{d}, the function will through an error.
+#' @return A new dataframe with the same columns as \code{d}.
+#' @note The exact time stamp of every data point is may depend, for instance, on the acquisition device's processing time.
+#' This it quite likely to result in irregular time series between devices.
+#' @examples
+#' \dontrun{
+#' FILE <- "result.db"
+#' # Load the three first ROIs
+#' ldfs <- loadROIsFromFile(FILE, rois=1:3)
+#' ###### Simple example resampling the first ROI from t=0 at 5Hz:
+#' d <- ldfs$ROI_1
+#' new_d <- interpolateROIData(d, fs=1, start_time=0)
+#' head(new_d)
+#' ###### Now resample from 0 to the last time point available at 1Hz, all dataframes.
+#' First, we get the last overall time point:
+#' last_time_point <- max(sapply(ldfs, function(d){d$t[length(d$t)]}))
+#' # then we use lapply to apply this function to all dataframes in the list
+#' resampled_dfs <- lapply(ldfs, interpolateROIData,  fs=1, start_time=0, stop_time=last_time_point)
+#' head(resampled_dfs$ROI_1)
+#'	}
+#' @seealso \code{\link{loadROIsFromFile}} in order to load ROI data.
 #' @export
 
-interpolateTS <- function(m,h=30,minRow=11){
-	
-	if(!any(class(m) == "matrix"))
-		stop("This function works with a matrix. If you have a a list of matrices, use lapply to call this function on each element of the list. See examples for details.")
+interpolateROIData <- function(d,min_n_points=11, fs=NA, start_time=NA, stop_time=NA){
+			
+	if(nrow(d) < min_n_points)
+		stop("This dataframe does not have enough rows to be resampled")
+		t0 <- d[1,'t']
+		tf <- d[nrow(d),'t']
+		if(!is.na(start_time))
+			t0 <- start_time
+		if(!is.na(stop_time))
+			tf <- stop_time
 		
-	
-	if(attributes(m)$tags.isHomogenous)
-		warning("This data matrix has already been resampled.")
 		
-	if(nrow(m) >= minRow & attributes(m)$tags.hasEnoughPoints){
-		t0 <- m[1,'time']
-		tf <- m[nrow(m),'time']
-		dt <- (tf-t0)/1000
-		n <- round(h*dt)
-		t_out <- seq(from = t0, to = tf, length.out=n)
-		
-		xx <- approx(x=m[,'time'], y=m[,'X'],xout=t_out)$y
+		if (is.na(fs))
+			fs <- median(diff(d[,"t"]))
 
-		yy <- approx(x=m[,'time'], y=m[,'Y'],xout=t_out)$y
+		dt <- (tf-t0)
 
-		L <- approx(x=m[,'time'], y=m[,'L'],xout=t_out)$y
+		t_out <- seq(from = t0, to = tf, by=1/fs)
 		
-		T <- approx(x=m[,'time'], y=m[,'Territory'],xout=t_out,method='const')$y
+		t_in <- d[,"t"]
 		
-		mm<-cbind(T,xx,yy,t_out,L)
-		atr <- attributes(m)
-		atr$dim <- attributes(mm)$dim
-		attributes(mm) <-atr
-		attributes(mm)$tags.hasEnoughPoints <- TRUE
-		}
-	else{
-		mm <- m
-		attributes(mm)$tags.hasEnoughPoints <- FALSE
-	}
-	attributes(mm)$tags.isHomogenous <- TRUE
-	mm
+		
+		new_d <- lapply(d[,colnames(d) != "t"],
+			function(v){
+				approx(x=t_in, y=v,xout=t_out)$y
+			}
+		)
+		new_d <- data.frame(t=t_out,new_d)
+		return(new_d)
 }
