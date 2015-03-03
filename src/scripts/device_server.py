dd21c5f14e54ea21443143dbdcfaf520c707d363
PolygonalTree <luis.garcia@polygonaltree.co.uk>
2015-03-03 13:46:37 +0000

    restart script test for development

--- src/scripts/device_server.py
+++ src/scripts/device_server.py
@@ -102,7 +102,8 @@ def update_system(id):
 
             except Exception as e:
                 logging.error(e)
-            subprocess.call(['bash','./restart.sh',str(os.getpid())])
+            run(reloader=True)
+            #subprocess.call(['bash','./restart.sh',str(os.getpid())])
             #os.execl('./device_server.py','-d')
 
 
