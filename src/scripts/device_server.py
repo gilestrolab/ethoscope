b490f4bc01d5899d0df7d4ead9c452216d1d196f
PolygonalTree <luis.garcia@polygonaltree.co.uk>
2015-03-03 15:15:44 +0000

    restart script test for development

--- src/scripts/device_server.py
+++ src/scripts/device_server.py
@@ -177,7 +177,7 @@ if __name__ == '__main__':
 
 
     try:
-        run(api, host='0.0.0.0', port=port, debug=debug)
+        run(api, host='0.0.0.0', port=port, debug=debug,)
     except Exception as e:
         logging.error(e)
     finally:
