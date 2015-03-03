fc8728da1baa7947661eadde0126545edf6e36fb
PolygonalTree <luis.garcia@polygonaltree.co.uk>
2015-03-03 14:38:32 +0000

    restart script test for development

--- psvnode_server/psvnode/scripts/server.py
+++ psvnode_server/psvnode/scripts/server.py
@@ -304,6 +304,7 @@ def check_update():
         #check connected devices
         scan_subnet()
         for key,d in devices_map.iteritems():
+            print "Version in Node-------->",d['version']
             if d['version'] != origin_version:
                 update[d['id']]= d
             #else:
