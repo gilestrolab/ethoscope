9b20bee4824482231d02289fb288f00c944fd2bd
PolygonalTree <luis.garcia@polygonaltree.co.uk>
2015-03-03 11:50:42 +0000

    restart script test for development

--- src/scripts/device_server.py
+++ src/scripts/device_server.py
@@ -95,7 +95,7 @@ def update_system(id):
 
             ##Need to restart the server. Scary thing.
             #Fixme this is for development, in the final version the script needs to restart the service
-            subprocess.Popen(['./restart.sh '+str(os.getpid())],shell=True,
+            subprocess.Popen(['./restart.sh ',str(os.getpid())],shell=True,
                              stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
 
