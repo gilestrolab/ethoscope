f233f841686d20b46b145998117776b857f4534e
PolygonalTree <luis.garcia@polygonaltree.co.uk>
2015-03-03 13:29:26 +0000

    restart script test for development

--- psvnode_server/psvnode/scripts/server.py
+++ psvnode_server/psvnode/scripts/server.py
@@ -269,7 +269,7 @@ def update_systems():
                 if error_from_fetch != '':
                     logging.error(error_from_fetch)
             else:
-                update_device_map(d['id'], what="update", data={'update':True})
+                update_device_map(d['id'], what="update", data={'update': 'true'})
 
     except Exception as e:
         return {'error':traceback.format_exc(e)}
