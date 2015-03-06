2c9802f44c20bbf9ee2486ed12c5c7c6e83ecc08
Quentin Geissmann <qgeissmann@gmail.com>
2015-03-06 11:33:27 +0000

    improve both accuracy and speed of monitor roi builders. Fixes #49

--- prototypes/automask_monitor/automask_monitor.py
+++ prototypes/automask_monitor/automask_monitor.py
@@ -1,5 +1,5 @@
 from pysolovideo.tracking.roi_builders import SleepMonitorWithTargetROIBuilder
 import cv2
 smrb = SleepMonitorWithTargetROIBuilder()
-im = cv2.imread("/tmp/b91dfd54-b15d-11e4-92e2-f62dad0aefc8.jpg")
+im = cv2.imread("./new_targets.png")
 smrb(im)
