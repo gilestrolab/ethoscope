109f6a4aa7cc0fd19972109266fb5b8ba70dd2c0
pepelisu <pepelisu@gmail.com>
2015-03-11 13:49:27 +0000

    change get-machine-id to a more general function

--- src/pysolovideo/web_utils/helpers.py
+++ src/pysolovideo/web_utils/helpers.py
@@ -4,26 +4,14 @@ import random
 import logging
 import traceback
 
-def get_machine_id():
-    """
-    Reads the machine ID file and returns the value.
-    """
-    f = open('/etc/machine-id', 'r')
-    pi_id = f.readline()
-    pi_id = pi_id.strip()
-    f.close()
-    return pi_id
-
-def get_machine_name():
+def get_machine_info(path):
     """
     Reads the machine NAME file and returns the value.
     """
     try:
-        f = open('/etc/machine-name', 'r')
-        pi_name = f.readline()
-        pi_name = pi_name.strip()
-        f.close()
-        return pi_name
+        with open(path,'r') as f:
+            info = f.readline().rstrip()
+        return info
     except Exception as e:
         logging.error(traceback.format_exc(e))
         return 'Debug-'+str(random.randint(1,100))
--- src/scripts/device_server.py
+++ src/scripts/device_server.py
@@ -5,7 +5,7 @@ import datetime
 from optparse import OptionParser
 from bottle import *
 from pysolovideo.web_utils.control_thread import ControlThread
-from pysolovideo.web_utils.helpers import get_machine_id, get_version, get_machine_name
+from pysolovideo.web_utils.helpers import get_machine_info, get_version
 from subprocess import call
 
 api = Bottle()
@@ -122,8 +122,6 @@ if __name__ == '__main__':
     debug = option_dict["debug"]
     port = option_dict["port"]
 
-    machine_id = get_machine_id()
-    machine_name = get_machine_name()
 
     INPUT_VIDEO = None
     DURATION = None
@@ -131,7 +129,11 @@ if __name__ == '__main__':
     PSV_DIR = "/psv_data/results"
     GIT_WORKING_DIR = '/home/psv/pySolo-Video'
     BRANCH = 'psv-package'
+    MACHINE_ID_FILE = '/etc/machine-id'
+    MACHINE_NAME_FILE = '/etc/machine-name'
 
+    machine_id = get_machine_info(MACHINE_ID_FILE)
+    machine_name = get_machine_name(MACHINE_NAME_FILE)
 
     if debug:
         import getpass
