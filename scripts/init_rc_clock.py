c247a3ab4df5fe9930c516aea8b50efc44a0636c
Quentin Geissmann <qgeissmann@gmail.com>
2015-03-19 12:55:06 +0000

    hwclock runj from init python script. Fixes #78?? attempt #N

--- scripts/init_rc_clock.py
+++ scripts/init_rc_clock.py
@@ -2,9 +2,14 @@ __author__ = 'quentin'
 import time
 import os
 
-with open("/sys/class/i2c-adapter/i2c-1/new_device", "w") as f:
-    f.write("ds1307 0x68\n")
-time.sleep(1)
-os.system('hwclock -s')
-print "time synced"
-print time.time()
+while True:
+    try:
+        with open("/sys/class/i2c-adapter/i2c-1/new_device", "w") as f:
+            f.write("ds1307 0x68\n")
+
+        os.system('hwclock -s')
+        print "time synced"
+        print time.time()
+
+    finally:
+        time.sleep(5)
