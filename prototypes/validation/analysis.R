52f1cf26463231a5af6478f265dc32dc03d1c233
Quentin Geissmann <qgeissmann@gmail.com>
2015-05-09 14:13:10 +0100

    go a bit deeper in analysis of behaviour

--- prototypes/validation/analysis.R
+++ prototypes/validation/analysis.R
@@ -1,41 +1,43 @@
-rm(list=ls())
+#'rm(list=ls())
 library(risonno)
 library(ggplot2)
 
+
 FILE <- "/data/validation/validation_out.db"
 ANNOT_RESULTS <- "/data/validation/rois_t_10s-chunks"
-behaviour_map <- list(w="walking", r="spinning", g="grooming", i="immobile")
-activity <- function(x,y){
-	comp = x + 1i*y
-	distance <- c(0, abs(diff(comp)))
-	return(distance)
+ANNOT_LEVELS <- c("walking","spinning","micro-mov.","immobile")
+BEHAVIOUR_MAP <- list(w="walking", r="spinning", g="micro-mov.", i="immobile")
+
+make_annotation_df <- function(annot_result_dir,behaviour_map,annot_levels){
+	files <- list.files(path=annot_result_dir, pattern="*.txt", full.names=T)
+	file_info <- do.call("rbind",strsplit(basename(files), "[_\\.]"))
+	ref <- as.data.table(file_info[,1:3])	
+	setnames(ref, colnames(ref),c("roi_id","t","user"))
+	ref[,roi_id:=as.numeric(roi_id)]
+	ref[,t:=as.numeric(t)]
+
+	annots <- rbindlist(lapply(files, fread))
+	setnames(annots, colnames(annots),c("behaviour","x","y"))
+	annots[,behaviour:=behaviour_map[behaviour]]
+	annots[,behaviour:=ordered(behaviour, levels=annot_levels)]
+	ref <- cbind(ref, annots)
+
+	setkeyv(ref, c("roi_id","t"))
+
+	ref <- ref[behaviour != 'n', ]
+	return(ref)
 }
 
-files <- list.files(path=ANNOT_RESULTS, pattern="*.txt", full.names=T)
-file_info <- do.call("rbind",strsplit(basename(files), "[_\\.]"))
-ref <- as.data.table(file_info[,1:3])	
-setnames(ref, colnames(ref),c("roi_id","t","user"))
-ref[,roi_id:=as.numeric(roi_id)]
-ref[,t:=as.numeric(t)]
+ref <- make_annotation_df(ANNOT_RESULTS, BEHAVIOUR_MAP, ANNOT_LEVELS )
 
-annots <- rbindlist(lapply(files, fread))
-setnames(annots, colnames(annots),c("behaviour","x","y"))
-annots[,behaviour:=behaviour_map[behaviour]]
-annots[,behaviour:=ordered(behaviour, levels=c("walking","spinning","grooming","immobile"))]
-ref <- cbind(ref, annots)
 
-setkeyv(ref, c("roi_id","t"))
-
-ref <- ref[behaviour != 'n', ]
-
+sink("./annotation_occurence.txt")
 print(xtabs( ~  behaviour, ref))
+sink()
+map <- data.frame(path=FILE, roi_id = unique(ref[,roi_id]))
+dt <- loadPsvData(map, FUN=sleepAnalysis)
 
- 
-files <- list.files(path="./results/rois_t_10s-chunks/", pattern="*.txt", full.names=T)
 
-
-dt <- loadROIsFromFile(FILE, rois = unique(ref[,roi_id]))
-dt[,activity:=activity(x,y) , by=key(dt)]
 
 
 pos_at_t = dt[t %in% unique(ref[,t]),list(t=t, xt=x, yt=y,roi_id=roi_id)]
@@ -43,11 +45,13 @@ setkeyv(pos_at_t,c('roi_id','t'))
 
 setkeyv(ref,c('roi_id','t'))
 
+stop("boom")
 
 pdt <- merge(pos_at_t, ref)
 pdt[,distance := abs((xt + 1i*yt) - (x +1i*y))]
 #todo invert axis -> food?
 pdf("/tmp/valitation.pdf",w=16,h=9)
+#'pl <- ggplot(pdt[behaviour != "walking"],aes(xt,x)) +
 pl <- ggplot(pdt,aes(xt,x)) +
 	geom_smooth(method='lm',formula=y~x) +
 	geom_point(aes(colour=behaviour, shape=behaviour, size=2, alpha=.5))
@@ -56,22 +60,45 @@ print(pl)
 mod <- lm(xt ~x, pdt)	
 print(summary(mod))
 
+o = copy(dt)
 
-d = copy(dt)
-sampling_period <- 10
-d[, t_round := sampling_period * (floor(d[,t] /sampling_period))]
-setkey(d, t_round)
-o <- d[,list(t0=min(t), tf=max(t),activity=sum(activity)),by=c("t_round","roi_id")]
+#'sampling_period <- 10
+#'d[, t_round := sampling_period * (floor(d[,t] /sampling_period))]
+#'setkey(d, t_round)
+#'
+#'
+#'o <- d[,list(
+#'			t0=min(t), 
+#'			tf=max(t),
+#'			cvrd_dist=sum(activity),
+#'			max_speed=max(activity),
+#'			ar_diff=sum(ar_diff),
+#'			phi_diff=sum(phi_diff)
+#'		),
+#'		by=c("t_round","roi_id")]
 	
 	
 
 
-act_at_t = o[t_round %in% unique(ref[,t]),list(t=t_round, activity=activity,roi_id=roi_id)]
+act_at_t = o[t %in% unique(ref[,t])]
+setnames(act_at_t, c("x","y"),c("xt","yt"))
 setkeyv(act_at_t,c('roi_id','t'))
 
 pdt2 <- merge(act_at_t, ref)
-pl <- ggplot(pdt2,aes(behaviour,activity,fill=behaviour)) +
+pl <- ggplot(pdt2,aes(behaviour,cvrd_dist,fill=behaviour)) +
 		geom_boxplot()  +  geom_hline(aes(yintercept=0.03)) +  scale_y_sqrt()
+
+pl <- ggplot(pdt2,aes(behaviour,ar_diff,fill=behaviour)) +
+		geom_boxplot()  +  geom_hline(aes(yintercept=0.03)) +  scale_y_sqrt()
+
+pl <- ggplot(pdt2,aes(behaviour,phi_diff,fill=behaviour)) +
+		geom_boxplot()  +  geom_hline(aes(yintercept=0.03)) +  scale_y_sqrt()
+
+pl <- ggplot(pdt2,aes(behaviour,max_velocity,fill=behaviour)) +
+		geom_boxplot()  +  geom_hline(aes(yintercept=0.006)) +  scale_y_sqrt()
+																
+ggplot(pdt2,aes(max_velocity,activity,shape=behaviour,colour=behaviour)) +
+		geom_point() + scale_y_sqrt() + scale_x_sqrt()
 print(pl)
 
 mod2 <- lm(sqrt(activity) ~ behaviour,pdt2)
@@ -79,3 +106,5 @@ print(summary(mod2))
 
 
 dev.off()
+
+
