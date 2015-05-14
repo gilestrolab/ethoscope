8280be8849a85a5d2c31de099bc7067c49e75697
Quentin Geissmann <qgeissmann@gmail.com>
2015-05-14 08:06:56 +0100

    playing with camera socket on devices

---
+++ prototypes/picamera_socket/server.py
@@ -0,0 +1,44 @@
+import io
+import socket
+import struct
+# from PIL import Image
+import cv2
+import numpy as np
+
+# Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
+# all interfaces)
+server_socket = socket.socket()
+server_socket.bind(('0.0.0.0', 8001))
+server_socket.listen(0)
+
+# Accept a single connection and make a file-like object out of it
+connection = server_socket.accept()[0].makefile('rb')
+try:
+    while True:
+        # Read the length of the image as a 32-bit unsigned int. If the
+        # length is zero, quit the loop
+        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+        if not image_len:
+            break
+        # Construct a stream to hold the image data and read the image
+        # data from the connection
+        image_stream = io.BytesIO()
+        image_stream.write(connection.read(image_len))
+        # Rewind the stream, open it as an image with PIL and do some
+        # processing on it
+        image_stream.seek(0)
+        # image = Image.open(image_stream)
+        # print('Image is %dx%d' % image.size)
+        # image.verify()
+        # print('Image is verified')
+
+        # # Construct a numpy array from the stream
+        # data = np.fromstring(image_stream.getvalue(), dtype=np.uint8)
+        # # "Decode" the image from the array, preserving colour
+        # image = cv2.imdecode(data, 1)
+        # # OpenCV returns an array with data in BGR order. If you want RGB instead
+        # # use the following...
+        # image = image[:, :, ::-1]
+finally:
+    connection.close()
+    server_socket.close()
