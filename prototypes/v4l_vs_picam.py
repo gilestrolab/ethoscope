e4286627f450c4222f713e2aca8d7c0f9acd19c5
Quentin Geissmann <qgeissmann@gmail.com>
2015-05-14 10:22:54 +0100

    try fixing issue using v4l2 cv2 frontend

--- prototypes/v4l_vs_picam.py
+++ prototypes/v4l_vs_picam.py
@@ -7,115 +7,70 @@ import time
 import time
 
 
-# capture = cv2.VideoCapture(0)
-# capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,640)
-# capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
-#
-# capture.set(cv2.cv.CV_CAP_PROP_FPS, 5)
-#
-# time.sleep(0.5)
-# #cv2.waitKey(2000)
-# _,im = capture.read()
-#
-#
-# NFRAMES = 1000
-# print "ok, frame shape=", im.shape
-#
-# t0 = time.time()
-# try:
-#     for _ in range(NFRAMES):
-#
-#         capture.grab()
-#         capture.retrieve(im)
-#
-#         cv2.imshow("frame", im)
-#         cv2.waitKey(1)
-#
-#
-#
-#         #im = np.copy(im)
-#         assert(len(im.shape) == 3)
-#     t1= time.time()
-#
-#     print (t1-t0) / float(NFRAMES)
-# finally:
-#     print "voila"
-#     capture.release()
-#
-# print "test"
-
-
 capture = cv2.VideoCapture(0)
-time.sleep(2)
-
-w, h = (640, 480)
-if w <0 or h <0:
-    capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 99999)
-    capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 99999)
-else:
-    capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, w)
-    capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, h)
+capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,1280)
+capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 960)
 
 capture.set(cv2.cv.CV_CAP_PROP_FPS, 5)
 
 
-time.sleep(1)
-_, im = capture.read()
-cv2.imshow("im", im); cv2.waitKey(-1)
+# CV_CAP_PROP_POS_MSEC Current position of the video file in milliseconds.
+# CV_CAP_PROP_POS_FRAMES 0-based index of the frame to be decoded/captured next.
+# CV_CAP_PROP_POS_AVI_RATIO Relative position of the video file: 0 - start of the film, 1 - end of the film.
+# CV_CAP_PROP_FRAME_WIDTH Width of the frames in the video stream.
+# CV_CAP_PROP_FRAME_HEIGHT Height of the frames in the video stream.
+# CV_CAP_PROP_FPS Frame rate.
+# CV_CAP_PROP_FOURCC 4-character code of codec.
+# CV_CAP_PROP_FRAME_COUNT Number of frames in the video file.
+# CV_CAP_PROP_FORMAT Format of the Mat objects returned by retrieve() .
+# CV_CAP_PROP_MODE Backend-specific value indicating the current capture mode.
+# CV_CAP_PROP_BRIGHTNESS Brightness of the image (only for cameras).
+# CV_CAP_PROP_CONTRAST Contrast of the image (only for cameras).
+# CV_CAP_PROP_SATURATION Saturation of the image (only for cameras).
+# CV_CAP_PROP_HUE Hue of the image (only for cameras).
+# CV_CAP_PROP_GAIN Gain of the image (only for cameras).
+# CV_CAP_PROP_EXPOSURE Exposure (only for cameras).
+# CV_CAP_PROP_CONVERT_RGB Boolean flags indicating whether images should be converted to RGB.
+# CV_CAP_PROP_WHITE_BALANCE_U The U value of the whitebalance setting (note: only supported by DC1394 v 2.x backend currently)
+# CV_CAP_PROP_WHITE_BALANCE_V The V value of the whitebalance setting (note: only supported by DC1394 v 2.x backend currently)
+# CV_CAP_PROP_RECTIFICATION Rectification flag for stereo cameras (note: only supported by DC1394 v 2.x backend currently)
+# CV_CAP_PROP_ISO_SPEED The ISO speed of the camera (note: only supported by DC1394 v 2.x backend currently)
+# CV_CAP_PROP_BUFFERSIZE
 
 
-capture.release()
+# CV_CAP_PROP_BRIGHTNESS Brightness of the image (only for cameras).
+# CV_CAP_PROP_CONTRAST Contrast of the image (only for cameras).
+# CV_CAP_PROP_SATURATION Saturation of the image (only for cameras).
+# CV_CAP_PROP_HUE Hue of the image (only for cameras).
+# CV_CAP_PROP_GAIN Gain of the image (only for cameras).
 
-device=0
-target_fps=1
-target_resolution=(640,480)
-capture = cv2.VideoCapture(device)
-w, h = target_resolution
-if w <0 or h <0:
-    capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 99999)
-    capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 99999)
-else:
-    capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, w)
-    capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, h)
-capture.set(cv2.cv.CV_CAP_PROP_FPS, target_fps)
-
-_target_fps = float(target_fps)
-time.sleep(1)
-_, im = capture.read()
-
-# preallocate image buffer => faster
-_frame = im
-
-cv2.imshow("im", im ); cv2.waitKey(-1)
+print cv2.cv.CV_CAP_PROP_BRIGHTNESS, capture.get(cv2.cv.CV_CAP_PROP_BRIGHTNESS)
+time.sleep(0.5)
+#cv2.waitKey(2000)
+_,im = capture.read()
 
 
+NFRAMES = 1000
+print "ok, frame shape=", im.shape
+
+t0 = time.time()
+try:
+    for _ in range(NFRAMES):
+
+        capture.grab()
+        capture.retrieve(im)
+
+        cv2.imshow("frame", im)
+        cv2.waitKey(1)
 
 
-#
-# _start_time = time.time()
-#
-# for i in range(300):
-#     expected_time =  _start_time +i / 5.0
-#     now = time.time()
-#
-#     to_sleep = expected_time - now
-#
-#     # Warnings if the fps is so high that we cannot grab fast enough
-#     if to_sleep < 0:
-#         print "The target FPS could not be reached. Frame lagging by  %f seconds" % (-1 * to_sleep)
-#         capture.grab()
-#
-#     # we simply drop frames until we go above expected time
-#     while now < expected_time:
-#         capture.grab()
-#         now = time.time()
-#     else:
-#         capture.grab()
-#
-#     capture.retrieve(im)
-#     cv2.imshow("im", im); cv2.waitKey(6)
-#
-#
-# #
-# # #TODO better exception handling is needed here / what do we do if initial capture fails...
-# # assert(len(im.shape) >1)
+
+        #im = np.copy(im)
+        assert(len(im.shape) == 3)
+    t1= time.time()
+
+    print (t1-t0) / float(NFRAMES)
+finally:
+    print "voila"
+    capture.release()
+
