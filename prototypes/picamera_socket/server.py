11f4f57c01ed6fb48a0baaaf759338fc6467b7c3
Quentin Geissmann <qgeissmann@gmail.com>
2015-05-15 10:08:59 +0100

    multiprocessing solution implementation 1

--- prototypes/picamera_socket/server.py
+++ prototypes/picamera_socket/server.py
@@ -1,18 +1,212 @@
 import numpy as np
-import picamera
-import picamera.array
+import logging
+try:
+    from picamera.array import PiRGBArray
+    from picamera import PiCamera
+except:
+    logging.warning("Could not load picamera module")
 
-class DetectMotion(picamera.array.PiRGBAnalysis):
+import multiprocessing
+from pysolovideo.tracking.cameras import BaseCamera
+from pysolovideo.utils.debug import PSVException
+import time
+
+
+
+
+# class PiFrameGrabber(Thread):
+class PiFrameGrabber(multiprocessing.Process):
+
+    def __init__(self, queue):
+
+        #self.camera...
+        res = (640,480)
+        self.img = np.zeros((res[0],res[1],3),np.uint8)
+        self._queue = queue
+
+        self._force_stop = False
+
+        super(PiFrameGrabber, self).__init__()
+
+    # def get_last_frame(self):
+    #     i = 0
+    #     msg = self._queue.get()
+    #     while self._queue.qsize()>1:
+    #         msg = self._queue.get()
+    #         print "FLUSHING", i
+    #     # print "no flushing", self._queue.qsize()
+    #     return msg
+
+
+
+
+    def stop(self):
+        self._queue.cancel_join_thread()
+        self._queue.close()
+
+
+    def run(self):
+        try:
+            self._frame_cb = FrameCallBack(None,self._queue)
+            ttt0 = time.time()
+            while not self._force_stop:
+
+                    time.sleep(.1)
+
+                    if self._frame_cb.is_done:
+                        TODO
+                    self._frame_cb.analyse(None)
+
+                    print "running222"
+                    # with picamera.PiCamera() as camera:
+                    #     with DetectMotion(camera) as output:
+                    #             self._img = output.get_array()
+                    #         camera.resolution = (640, 480)
+                    #         camera.start_recording(output, format='bgr')
+                    #         camera.wait_recording(3600*24*365)
+                    #         camera.stop_recording()
+                    # if time.time() - ttt0 > 3:
+                    #     raise Exception("bouya")
+
+        finally:
+            self._queue.cancel_join_thread()
+            pass
+
+
+class OurPiCamera(BaseCamera):
+
+    def __init__(self, target_fps=10, target_resolution=(960,720), *args, **kwargs):
+
+        logging.info("Initialising camera")
+        w,h = target_resolution
+        self.capture = PiCamera()
+
+        self.capture.resolution = target_resolution
+        if not isinstance(target_fps, int):
+            raise PSVException("FPS must be an integer number")
+
+        self.capture.framerate = target_fps
+
+        self._raw_capture = PiRGBArray(self.capture, size=target_resolution)
+
+        self._target_fps = float(target_fps)
+        self._warm_up()
+
+        self._cap_it = self._frame_iter()
+
+        im = next(self._cap_it)
+
+        if im is None:
+            raise PSVException("Error whist retrieving video frame. Got None instead. Camera not plugged?")
+
+        self._frame = im
+
+
+
+        if len(im.shape) < 2:
+            raise PSVException("The camera image is corrupted (less that 2 dimensions)")
+
+        self._resolution = (im.shape[1], im.shape[0])
+        if self._resolution != target_resolution:
+            if w > 0 and h > 0:
+                logging.warning('Target resolution "%s" could NOT be achieved. Effective resolution is "%s"' % (target_resolution, self._resolution ))
+            else:
+                logging.info('Maximal effective resolution is "%s"' % str(self._resolution))
+
+
+        super(OurPiCamera, self).__init__(*args, **kwargs)
+
+        self._start_time = time.time()
+        logging.info("Camera initialised")
+
+    def _warm_up(self):
+        logging.info("%s is warming up" % (str(self)))
+        time.sleep(1)
+
+    def restart(self):
+        self._frame_idx = 0
+        self._start_time = time.time()
+
+    def is_opened(self):
+        return True
+        # return self.capture.isOpened()
+
+
+    def is_last_frame(self):
+        return False
+
+    def _time_stamp(self):
+        now = time.time()
+        # relative time stamp
+        return now - self._start_time
+
+    @property
+    def start_time(self):
+        return self._start_time
+
+    def _close(self):
+        self._raw_capture.truncate(0)
+        self.capture.close()
+
+        # self.capture.release()
+
+    def _frame_iter(self):
+
+        # capture frames from the camera
+
+        for frame in self.capture.capture_continuous(self._raw_capture, format="bgr", use_video_port=True):
+            # grab the raw NumPy array representing the image, then initialize the timestamp
+            # and occupied/unoccupied text
+
+        # clear the stream in preparation for the next frame
+            self._raw_capture.truncate(0)
+            yield frame.array
+
+
+class FrameCallBack(object):
+    def __init__(self,camera,queue):
+        # res = camera.resolution
+        self._queue = queue
+        self.is_done=False
+        # super(FrameCallBack, self).__init__(camera)
+        super(FrameCallBack, self).__init__()
+
     def analyse(self, a):
+        out = np.random.random((640,480,3))
+        out *=100
+        out = out.astype(np.uint8)
+        while self._queue.qsize()>1:
+            msg = self._queue.get()
+            if msg=="DONE":
+                self.is_done=True
+        self._queue.put(out)
 
-        # If there're more than 10 vectors with a magnitude greater
-        # than 60, then say we've detected motion
-        print np.sum(a)
 
-with picamera.PiCamera() as camera:
-    with DetectMotion(camera) as output:
-        camera.resolution = (640, 480)
-        camera.start_recording(
-              '/dev/null', format='bgr')
-        camera.wait_recording(30)
-        camera.stop_recording()
+
+
+
+_queue = multiprocessing.JoinableQueue()
+p = PiFrameGrabber(_queue)
+p.daemon = False
+p.start()
+
+# if time.time() - ttt0 > 3:
+#     raise Exception("bouya")
+
+loop=True
+try:
+    ttt0 = time.time()
+    while loop:
+
+            # p.get_last_frame()[1,1]
+            time.sleep(.5)
+
+            if time.time() - ttt0 > 3:
+                loop=False
+                print "OUTOFLOOP"
+except:
+    pass
+finally:
+    p.stop()
+    print "join"
+    p.join()
---
+++ prototypes/picamera_socket/sub_process.py
@@ -0,0 +1,179 @@
+__author__ = 'quentin'
+
+import numpy as np
+import logging
+try:
+    from picamera.array import PiRGBArray
+    from picamera import PiCamera
+except:
+    logging.warning("Could not load picamera module")
+
+import multiprocessing
+from pysolovideo.tracking.cameras import BaseCamera
+from pysolovideo.utils.debug import PSVException
+import time
+
+
+
+
+class PiFrameGrabber(multiprocessing.Process):
+
+    def __init__(self, target_fps, target_resolution, queue):
+        self._queue = queue
+        self._target_fps = target_fps
+        self._target_resolution = target_resolution
+
+
+        super(PiFrameGrabber, self).__init__()
+
+
+    def run(self):
+        try:
+            w,h = self._target_resolution
+            capture = PiCamera()
+            capture.resolution = self._target_resolution
+
+            capture.framerate = self._target_fps
+            raw_capture = PiRGBArray(capture, size=self._target_resolution)
+
+            for frame in capture.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+
+
+                to_break = False
+                while not self._queue.empty():
+                    msg = self._queue.get()
+                    if msg is None:
+                        to_break =True
+                if to_break:
+                    logging.info("Camera frame grabber was instructed to stop by parent process")
+                    break
+                raw_capture.truncate(0)
+                out = np.copy(frame.array)
+                self._queue.put(out)
+
+
+        finally:
+            capture.close()
+
+
+
+
+class OurPiCameraAsync(BaseCamera):
+
+    def __init__(self, target_fps=10, target_resolution=(960,720), *args, **kwargs):
+
+        logging.info("Initialising camera")
+        w,h = target_resolution
+
+        if not isinstance(target_fps, int):
+            raise PSVException("FPS must be an integer number")
+
+
+
+        self._queue = multiprocessing.Queue()
+        self._p = PiFrameGrabber(target_fps,target_resolution,self._queue)
+        self._p.daemon = True
+        self._p.start()
+
+
+        self._cap_it = self._frame_iter()
+
+        im = next(self._cap_it)
+
+        if im is None:
+            raise PSVException("Error whist retrieving video frame. Got None instead. Camera not plugged?")
+
+        self._frame = im
+
+        if len(im.shape) < 2:
+            raise PSVException("The camera image is corrupted (less that 2 dimensions)")
+
+        self._resolution = (im.shape[1], im.shape[0])
+        if self._resolution != target_resolution:
+            if w > 0 and h > 0:
+                logging.warning('Target resolution "%s" could NOT be achieved. Effective resolution is "%s"' % (target_resolution, self._resolution ))
+            else:
+                logging.info('Maximal effective resolution is "%s"' % str(self._resolution))
+
+
+        super(OurPiCameraAsync, self).__init__(*args, **kwargs)
+
+        self._start_time = time.time()
+        logging.info("Camera initialised")
+
+    def _warm_up(self):
+        logging.info("%s is warming up" % (str(self)))
+        time.sleep(1)
+
+    def restart(self):
+        self._frame_idx = 0
+        self._start_time = time.time()
+
+    def is_opened(self):
+        return True
+        # return self.capture.isOpened()
+
+
+    def is_last_frame(self):
+        return False
+
+    def _time_stamp(self):
+        now = time.time()
+        # relative time stamp
+        return now - self._start_time
+
+    @property
+    def start_time(self):
+        return self._start_time
+
+    def _close(self):
+
+        self._queue.put(None)
+        self._p.join(timeout=5)
+
+    def _frame_iter(self):
+        try:
+            while True:
+                o = self._queue.get(timeout=1)
+                self._frame_idx +=1
+                yield o
+        except:
+            raise Exception("Could not get frame from camera")
+
+
+#
+# class CameraStub(object):
+#     def __init__(self):
+#         self._stop = False
+#         self._queue = multiprocessing.Queue()
+#         self._p = PiFrameGrabber(self._queue)
+#         self._p.daemon = True
+#         self._p.start()
+#
+#
+#     def frame_iter(self):
+#         try:
+#             while not self._stop:
+#                 print "qs = ", self._queue.qsize()
+#                 o = self._queue.get(timeout=1)
+#                 yield o
+#         except:
+#             raise Exception("Could not get frame from camera")
+#     def close(self):
+#         self._stop = True
+#         self._queue.put(None)
+#         self._p.join(timeout=5)
+#
+#
+# c = CameraStub()
+# t0 = time.time()
+# for i in c.frame_iter():
+#     print i
+#     time.sleep(.5)
+#     if time.time() - t0 > 5:
+#         break
+# c.close()
+#
+c = OurPiCameraAsync(target_fps=5,target_resolution=(1280,960))
+for t,f in c:
+    print t,f.shape
