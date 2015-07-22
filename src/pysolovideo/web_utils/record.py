4e1933a16b64e74a665593c60c0ec82dd4d8cd4d
PolygonalTree <luis.garcia@polygonaltree.co.uk>
2015-07-22 20:08:59 +0100

    adds path join

--- src/pysolovideo/web_utils/record.py
+++ src/pysolovideo/web_utils/record.py
@@ -1,19 +1,20 @@
 import picamera
+from os import path
 from threading import Thread
 
 class RecordVideo(Thread):
 
-    def __init__(self, resolution=(640,480), framerate=24, name="myvideo",  PSV_DIR = "/psv_data/results"):
+    def __init__(self, resolution=(640,480), framerate=24, name="/myvideo",  PSV_DIR = "/psv_data/results"):
+        super(RecordVideo, self).__init__()
         self.camera = picamera.PiCamera()
         self.camera.resolution = resolution
         self.camera.framerate = framerate
-        self.name = name
-        self.psv_dir = PSV_DIR
-        super(RecordVideo, self).__init__()
+        self.save_dir = path.join(PSV_DIR, name)
+
 
     def run(self):
         try:
-            self.camera.start_recording(self.psv_dir + self.name + '.h264')
+            self.camera.start_recording(self.save_dir + '.h264')
         except Exception as e:
             print (e)
 
