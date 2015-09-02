40732a780ab55a8cbb35bc7475d6c57b826bbd46
Quentin Geissmann <qgeissmann@gmail.com>
2015-09-02 18:13:51 +0100

    fixes #19 !

--- src/ethoscope/interactors/sleep_depriver_interactor.py
+++ src/ethoscope/interactors/sleep_depriver_interactor.py
@@ -42,8 +42,6 @@ class IsMovingInteractor(BaseInteractorSync):
         dist = 10.0 ** (tail_m["xy_dist_log10x1000"]/1000.0)
         velocity = dist / dt_s
 
-        if roi_id == 6:
-            print "velocity", velocity, "dist",dist,"dt_s",dt_s, "threshold",self._velocity_threshold
         if velocity > self._velocity_threshold:
             return True
         return False
@@ -110,9 +108,7 @@ class SleepDepInteractor(IsMovingInteractor):
 
         roi_id= self._tracker._roi.idx
         now =  self._tracker.last_time_point
-        
-        if roi_id==6:
-            print "t =", now
+
         try:
             channel = self._roi_to_channel[roi_id]
         except KeyError:
@@ -128,8 +124,6 @@ class SleepDepInteractor(IsMovingInteractor):
         if self._t0 is None:
             self._t0 = now
 
-        if roi_id==6:
-            print "t =", now - self._t0, "has_moved = ", has_moved
         if not has_moved:
             if float(now - self._t0) > self._inactivity_time_threshold_ms:
                 self._t0 = None
