6ffcfdedc6bfdbeb204d8a946a761b3c303031fd
Quentin Geissmann <qgeissmann@gmail.com>
2015-09-16 18:11:31 +0100

    nice progress on the independent updater

--- prototypes/independent_update/updater.py
+++ prototypes/independent_update/updater.py
@@ -1,7 +1,11 @@
 __author__ = 'quentin'
 
 from git import Repo
+from git import GitCommandError
 import logging
+import traceback
+
+
 
 
 class BaseUpdater(object):
@@ -17,6 +21,11 @@ class BaseUpdater(object):
 
 
     def get_local_and_origin_commits(self):
+        """
+        Gets the id of the local head and the origin's.
+        returned object are Commits, not strings. their unix TS can be access through``.committed_date``.
+        """
+
         local_commit = self._working_repo.commit()
         active_branch = self._working_repo.active_branch
         origin_commit = self._origin.refs[str(active_branch)].commit
@@ -24,6 +33,9 @@ class BaseUpdater(object):
         return local_commit, origin_commit
 
     def update_active_branch(self):
+        """
+        Pull the new available files obly for the current branch. Then checks that commit matches
+        """
         logging.info("Pulling origin")
         self._origin.pull()
         logging.info("Pulled")
@@ -35,13 +47,29 @@ class BaseUpdater(object):
             raise Exception(msg)
 
     def available_branches(self):
+        """
+        Lists available branches. Useful in order to offer user the possibility to change branch.
+
+        """
         self._origin.fetch()
         refs = self._origin.refs
         return [str(r).split("/")[-1] for r in refs[1:]]
 
     def change_branch(self, branch):
+        """
+        Change WD to a branch
+
+        :param branch: name of the branch
+        :type branch: str
+        """
+
         self._origin.fetch()
         self._working_repo.git.checkout(branch)
+
+    def restart_daemon(self):
+        # TODO
+        # I am not sure this class should actually deal with restarting any daemon. this should be handled by the parent bottle server
+        pass
 
 
 class DeviceUpdater(BaseUpdater):
@@ -49,8 +77,58 @@ class DeviceUpdater(BaseUpdater):
     pass
 
 
+class BareRepoUpdater(object):
+    """
+    A simple class to update the bare repo.
+    All visible branches are updated.
+    Hidden branches can be unlocked bu developers.
+    """
+    def __init__(self,git_working_dir):
+        self._git_working_dir = git_working_dir
+        self._working_repo = Repo(git_working_dir)
+        self._origin = self._working_repo.remotes.origin
+
+
+    def update_all_visible_branches(self):
+        """
+        Update all visible branches (for normal users, this will be master and dev).
+        For developers, new branches can be discovered (see ``discover_branches``).
+        :return:
+        """
+        branches = self._working_repo.branches
+
+        for b in branches:
+            try:
+                self._origin.fetch(b)
+            except GitCommandError as e:
+                logging.error(traceback.format_exc(e))
+
+
+
+    def discover_branches(self):
+        """
+        Force bare to discover new branches in the remote and sync them locally
+        :return:
+        """
+        try:
+            self._working_repo.git.fetch("origin","*:*")
+        except GitCommandError as e:
+            logging.error(traceback.format_exc(e))
+
+
+
+
+# #This works, but mind root permission
+# bare_updater = BareRepoUpdater("/srv/git/dummy_repo_bare.git")
+# print bare_updater._working_repo.branches
+# bare_updater.discover_branches()
+# bare_updater.update_all_visible_branches()
+#
+
+
+#
 updater = DeviceUpdater("/tmp/dummy_repo")
 print updater.available_branches()
-updater.change_branch("dev")
+updater.change_branch("another_branch")
 updater.update_active_branch()
-
+#
