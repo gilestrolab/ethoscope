b21d8cafeaecf9dae06a85f7400373f2c3af9842
Quentin Geissmann <qgeissmann@gmail.com>
2016-01-21 19:09:23 +0000

    improve setup

--- src/setup.py
+++ src/setup.py
@@ -1,34 +1,36 @@
-from distutils.core import setup
+from distutils.core import setup, find_packages
 
 setup(
     name='ethoscope',
-    version='trunk',
+    version='1.0.0',
     author=['Quentin Geissmann', 'Giorgio Gilestro', 'Luis Garcia'],
     author_email= ['quentin.geissmann13@imperial.ac.uk','g.gilestro@imperial.ac.uk', 'luis.garcia@polygonaltree.co.uk'],
-    packages=[ 'ethoscope',
-               'ethoscope.core',
-               'ethoscope.hardware',
-               'ethoscope.hardware.input',
-               'ethoscope.hardware.interfaces',
-               'ethoscope.interactors',
-               'ethoscope.roi_builders',
-               'ethoscope.trackers',
-               'ethoscope.utils',
-               'ethoscope.web_utils'
-              ],
+    packages=find_packages(),
     url="https://github.com/gilestrolab/ethoscope",
     license="GPL3",
     description='The API of the Ethoscope device.', #TODO
-    long_description=open('README').read(),
+    long_description="TODO",
 
+    keywords=["behaviour", "video tracking"],
     scripts=['scripts/device_server.py'],
 
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Topic :: Scientific/Engineering'
+    ],
     # data e.g. classifiers can be added as part of the package
     # TODO
     # package_data={'ethoscope': ['data/classifiers/*.pkl']},
     # extras_require={
     #     'pipes': ['picamera>=1.8'],
+    #      "GitPython >=1.0.1"
+    #         "MySQL-python >= 1.2.5",
+    #         "cherrypy >= 3.6.0",
     # },
+    setup_requires=[
+        "numpy>=1.6.1"
+        ],
     install_requires=[
         "numpy>=1.6.1",
         "pyserial>=2.7",
@@ -38,4 +40,6 @@ setup(
         "scipy >= 0.15.1",
         "GitPython >=1.0.1"
     ],
+    tests_require=['nose'],
+    test_suite='nose.collector'
 )
