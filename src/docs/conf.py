cb5df41ef1cf231b85ba41d5fb2b87813f15831d
Quentin Geissmann <qgeissmann@gmail.com>
2016-01-21 19:08:20 +0000

    make a readthedoc firendly doc config

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -18,7 +18,8 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
+
 
 # -- General configuration ------------------------------------------------
 
@@ -349,3 +350,4 @@ epub_exclude_files = ['search.html']
 # If false, no index is generated.
 #epub_use_index = True
 autoclass_content = "both"
+
