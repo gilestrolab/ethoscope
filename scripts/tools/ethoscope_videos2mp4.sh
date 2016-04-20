f0e0e3a5d0c4cad458bd697dcf695a42370c0d44
Quentin Geissmann <qgeissmann@gmail.com>
2016-04-20 17:23:28 +0100

    fix video merging script

--- scripts/tools/ethoscope_videos2mp4.sh
+++ scripts/tools/ethoscope_videos2mp4.sh
@@ -5,9 +5,9 @@ cd $1
 TMP_FILE=chunks.tmp
 echo "Merging $(ls *.h264 | wc -l) h264 chunks in tmp file"
 cat  *.h264  > $TMP_FILE
-fps=$(ls *.h264 | head -n 1 | cut -d _ -f 4 | cut -d @ -f 2)
+fps=$(ls *.h264 | head -n 1 | cut -d _ -f 5 | cut -d @ -f 2)
 prefix="whole_$(ls *.h264 | head -n 1 | cut -d . -f 1)"
 echo "Using ffmpeg to create $(pwd)/$prefix.mp4"
 ffmpeg -r $fps -i $TMP_FILE  -vcodec copy -y  $prefix.mp4  -loglevel panic
-rm chunks.tmp
+rm $TMP_FILE
 
