7bf9fc0f6aaab51cf3b37e22642e5293f569f4d8
luis.garcia <luis.garcia@uni-muenster.de>
2017-05-12 12:48:48 +0200

    sove problem in morecontroller.js missing bracket

--- node_src/static/js/controllers/moreController.js
+++ node_src/static/js/controllers/moreController.js
@@ -110,13 +110,13 @@
             }
         };
         bytesToSize = function (bytes) {
-   if(bytes == 0) return '0 Byte';
-   var k = 1000;
-   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-   var i = Math.floor(Math.log(bytes) / Math.log(k));
-   return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+	   if(bytes == 0) return '0 Byte';
+	   var k = 1000;
+	   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+	   var i = Math.floor(Math.log(bytes) / Math.log(k));
+	   return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+	}
+
 }
-
-
  angular.module('flyApp').controller('moreController',moreController);
 })()
