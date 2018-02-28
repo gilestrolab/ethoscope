73530ca8336c80b767a5342f52c38a4f5dae62bf
Quentin Geissmann <qgeissmann@gmail.com>
2018-02-28 08:18:32 -0800

    Update optomotor_stimulators.py

--- src/ethoscope/stimulators/optomotor_stimulators.py
+++ src/ethoscope/stimulators/optomotor_stimulators.py
@@ -24,3 +24,15 @@ class OptoMidlineCrossStimulator (MiddleCrossingStimulator):
         20: 15
     }
 
+
+class MotoMidlineCrossStimulator (MiddleCrossingStimulator):
+    _description = {"overview": "A stimulator to turn gear motor when animals cross the midline",
+                    "arguments": [
+                                    {"type": "number", "min": 0.0, "max": 1.0, "step":0.01, "name": "p", "description": "the probability to move the tube when a beam cross was detected","default":1.0},
+                                    {"type": "date_range", "name": "date_range",
+                                     "description": "A date and time range in which the device will perform (see http://tinyurl.com/jv7k826)",
+                                     "default": ""}
+                                   ]}
+    _HardwareInterfaceClass = OptoMotor
+    _roi_to_channel = {1:0, 3:2, 5:4, 7:6, 9:8,
+    12:22, 14:20, 16:18, 18:16, 20:14}
