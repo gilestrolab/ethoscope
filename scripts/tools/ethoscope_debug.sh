2f6fd0ae583b2cb30dc3db1f7a9ead8f5c8b03c0
Quentin Geissmann <qgeissmann@gmail.com>
2018-11-30 12:19:24 -0800

    add pip freeze to debuging script: installed packages and versions

--- scripts/tools/ethoscope_debug.sh
+++ scripts/tools/ethoscope_debug.sh
@@ -3,6 +3,7 @@ FILENAME=ethoscope_debug.log
 uname -a > ${FILENAME}
 ifconfig -a >> ${FILENAME}
 python --version >> ${FILENAME}
+pip freeze >> ${FILENAME}
 ping -c 1 8.8.8.8 >> ${FILENAME}
 systemctl status {ethoscope_node, ethoscope_backup, 
 ethoscope_results.mount}, 
