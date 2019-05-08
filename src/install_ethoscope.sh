4f1c7ea7adc19792f33c7be697f1b462e20e7068
Giorgio Gilestro <giorgio@gilest.ro>
2019-05-08 12:51:33 +0100

    Update install_ethoscope.sh
    Fixes the output of the machine id to a format that is compatible with what is expected (i.e. 32 characters, no dashes). A wrongly formatted machine-id would be overwritten otherwise.

--- src/install_ethoscope.sh
+++ src/install_ethoscope.sh
@@ -213,8 +213,8 @@ sync
 mv ${TMP_DIR}/root/boot/* ${TMP_DIR}/boot/
 
 echo "now writing the information regarding ID"
-UUID=`blkid $PART1 | egrep '[0-9A-F]{4}-[0-9A-F]{4}' -o`
-echo ${ID}-${UUID} > ${TMP_DIR}/root/etc/machine-id
+mid="$ID`uuidgen`"
+echo ${mid//-/} | cut -c 1-32 > ${TMP_DIR}/root/etc/machine-id
 echo "ETHOSCOPE_$ID" > ${TMP_DIR}/root/etc/machine-name
 echo "ETHOSCOPE_$ID" > ${TMP_DIR}/root/etc/hostname
 
