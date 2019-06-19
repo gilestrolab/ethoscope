28df4ebb375720782a7f9d5bc87a9382d2eaa3b8
ggilestro <giorgio@gilest.ro>
2019-06-19 13:22:42 +0100

    Adds the submodule utils so that we can install the full thing using setuptools too Updates authors email addresses Changes dependency from mysql-python to mysql-connector

--- node_src/setup.py
+++ node_src/setup.py
@@ -4,11 +4,11 @@ setup(
     name='ethoscope_node',
     version='trunk',
     author=['Quentin Geissmann', 'Giorgio Gilestro', 'Luis Garcia'],
-    author_email=['quentin.geissmann13@imperial.ac.uk', 'g.gilestro@imperial.ac.uk', 'luis.garcia@polygonaltree.co.uk'],
-    packages=['ethoscope_node'],
+    author_email=['quentin.geissmann@gmail.com', 'giorgio@gilest.ro', 'luis.garcia@uni-muenster.de'],
+    packages=['ethoscope_node', 'ethoscope_node.utils'],
     url="https://github.com/gilestrolab/ethoscope",
     license="GPL3",
-    description='todo',  #TODO
+    description='Ethoscope node server Python utils- http://lab.gilest.ro/ethoscope',
     long_description="TODO", # TODO open('README').read(),
     # data e.g. classifiers can be added as part of the package
     # TODO
@@ -18,7 +18,8 @@ setup(
     # },
     install_requires=[
         "bottle>=0.12.8",
-        "MySQL-python >= 1.2.5",
+#        "MySQL-python >= 1.2.5",
+        "mysql-connector-python >= 8.0.16",
         "netifaces >= 0.10.4",
         "cherrypy >= 3.6.0",
         "eventlet >= 0.17.1",
