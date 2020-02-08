ff9cb04b9f506ebb950e7b53f7f8b8bca5dcf79d
ggilestro <giorgio@gilest.ro>
2020-02-08 22:53:29 +0000

    Implemented pagination for the runs table, considering that it is probably going to become very long - TODO: fix search of the entries so that only positive entries will be paginated

--- node_src/static/js/controllers/experimentsController.js
+++ node_src/static/js/controllers/experimentsController.js
@@ -3,25 +3,40 @@
     var experimentsController = function($scope, $http, $timeout, $routeParams, $window, $interval) {
 
         $scope.currentPage = 0;
+        $scope.itemsPerPage = 20;
+
+        const range = (start, stop, step = 1) =>
+          Array(Math.ceil((stop - start) / step)).fill(start).map((x, y) => x + y * step)
+
+        updateRange = function () {
+            var start = 0;
+            var end = start + 7;
+            
+            ($scope.currentPage < 3) ? (start = 0) : (start = $scope.currentPage - 3);
+            ($scope.pageNumber - $scope.currentPage < 4) ? (end = $scope.pageNumber) : (end = start + 7);
+            $scope.pageRange = range(start, end);
+        }
         
         $scope.prevPage = function () {
             if ($scope.currentPage > 0) {
                 $scope.currentPage--;
+                updateRange();
             }
         };
         
         $scope.nextPage = function () {
-            if ($scope.currentPage < $scope.pagedRuns.length - 1) {
+            if ($scope.currentPage < $scope.pageNumber - 1) {
                 $scope.currentPage++;
+                updateRange();
             }
         };
-        
+
         $scope.setPage = function () {
             $scope.currentPage = this.n;
+            updateRange();
         };
 
-
-
+        
         // We need to make this call syncronous because the server may take a while to generate these data
         $scope.getBackupInfo = function () { return $http.get('/browse/null') };
 
@@ -51,10 +66,31 @@
                     else
                         { $scope.runs[ix].has_backup = false };
                 }
-
+                $scope.groupToPages();
             });
+        });
 
-                });
+        // calculate page in place
+        $scope.groupToPages = function () {
+
+            $scope.pages = [];
+            $scope.runsArray = [];
+            
+            //transform runs from dict to array
+            for (var run in $scope.runs) {
+                if ($scope.runs.hasOwnProperty(run)) {
+                    $scope.runsArray.push( $scope.runs[run] );
+                }
+            }
+
+            $scope.pageNumber = Math.ceil ( $scope.runsArray.length / $scope.itemsPerPage );
+            updateRange();
+            
+            for (var p = 0; p < $scope.pageNumber; p++) {
+                $scope.pages[p] = $scope.runsArray.slice(p * $scope.itemsPerPage, (p+1) * $scope.itemsPerPage -1 );
+            }
+            
+        };
 
         $scope.compare_time = function (t1, t2) {
             //compare two dates
