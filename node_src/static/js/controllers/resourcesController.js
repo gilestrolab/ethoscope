9ae58e8fdabeb694c1d4968baa921c534a3e140f
ggilestro <giorgio@gilest.ro>
2020-02-16 18:52:52 +0000

    Looks for resource files online - in this way when I update the resource I do not have to update the git repo

--- node_src/static/js/controllers/resourcesController.js
+++ node_src/static/js/controllers/resourcesController.js
@@ -1,7 +1,20 @@
 (function(){
     var resourcesController = function($scope, $http, $timeout, $routeParams, $window){
+        
+        var URL = "http://lab.gilest.ro:8001/resources";
+        $scope.resources = {};
+        $scope.announcement = "";
+        $scope.hasAccess = false;
+        
+        $http.get(URL)
+             .success(function(data, status, headers, config){
+                $scope.resources = data;
+                $scope.hasAccess = true;
+        });
+        
 }
 
  angular.module('flyApp').controller('resourcesController',resourcesController);
 
 })()
+
