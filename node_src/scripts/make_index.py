4d8a561c886db91565426cca0b4574b4a1321c01
Giorgio Gilestro <giorgio@gilest.ro>
2020-12-04 16:35:59 +0000

    Changes the form in make_index.py

--- node_src/scripts/make_index.py
+++ node_src/scripts/make_index.py
@@ -3,17 +3,21 @@
 import fnmatch
 import os
 
-path = '/ethoscope_data/results/'
-index_file = os.path.join(path, "index.txt")
+def make_index_file(path = '/ethoscope_data/results/'):
 
-matches = []
-for root, dirnames, filenames in os.walk(path):
-    for filename in fnmatch.filter(filenames, '*.db'):
-        matches.append( os.path.join(root, filename) )
-        
-with open(index_file, "w") as ind:
+    index_file = os.path.join(path, "index.txt")
     
-    for db in matches:
-        fp = os.path.relpath(db, path )
-        fs = os.stat(fp).st_size
-        ind.write('"%s", %s\n' % (fp, fs))
+    matches = []
+    for root, dirnames, filenames in os.walk(path):
+        for filename in fnmatch.filter(filenames, '*.db'):
+            matches.append( os.path.join(root, filename) )
+            
+    with open(index_file, "w") as ind:
+        
+        for db in matches:
+            fp = os.path.relpath(db, path )
+            fs = os.stat(fp).st_size
+            ind.write('"%s", %s\n' % (fp, fs))
+
+if __name__ == '__main__':
+    make_index_file()
