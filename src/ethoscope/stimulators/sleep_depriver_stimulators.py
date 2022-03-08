803a9caca74657731a800273e5b020ada900162a
L.Blackhurst <57448494+Lblackhurst29@users.noreply.github.com>
2022-03-08 15:01:57 +0000

    Changed step for probability setting of class SleepDepStimulator

--- src/ethoscope/stimulators/sleep_depriver_stimulators.py
+++ src/ethoscope/stimulators/sleep_depriver_stimulators.py
@@ -76,7 +76,7 @@ class SleepDepStimulator(IsMovingStimulator):
                     "arguments": [
                                     {"type": "number", "min": 0.0, "max": 1.0, "step":0.0001, "name": "velocity_correction_coef", "description": "Velocity correction coef","default":3.0e-3},
                                     {"type": "number", "min": 1, "max": 3600*12, "step":1, "name": "min_inactive_time", "description": "The minimal time after which an inactive animal is awaken","default":120},
-                                    {"type": "number", "min": 0.0, "max": 1.0, "step": 0.1, "name": "stimulus_probability",  "description": "Probability the stimulus will happen", "default": 1.0},
+                                    {"type": "number", "min": 0.0, "max": 1.0, "step": 0.01, "name": "stimulus_probability",  "description": "Probability the stimulus will happen", "default": 1.0},
                                     {"type": "date_range", "name": "date_range",
                                      "description": "A date and time range in which the device will perform (see http://tinyurl.com/jv7k826)",
                                      "default": ""},
