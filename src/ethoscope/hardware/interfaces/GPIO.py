ae0293db34aad3d5a2c39dc8f50cce078658e370
Giorgio Gilestro <giorgio@gilest.ro>
2022-03-17 18:44:06 +0000

    Modifies GPIO so that can be used more easily from any other script

--- src/ethoscope/hardware/interfaces/GPIO.py
+++ src/ethoscope/hardware/interfaces/GPIO.py
@@ -7,6 +7,9 @@ import logging
 import json, ast
 
 GPIO.setmode(GPIO.BOARD)
+DEFAULT_JSON_FILE = "/etc/gpio.conf"
+
+
 
 class Output():
     '''
@@ -39,16 +42,57 @@ class GPIOButtons():
     
     _ALLOWED_GPIOS = [7,11,12,13,15,16,18,22,29,31,32,33,35,36,37,38,40]
 
-    def __init__(self, commands):
+    def __init__(self, commands=None, filename=DEFAULT_JSON_FILE):
         
-        self.BTN = [ Button ( int(b), commands[b]) for b in commands if int(b) in self._ALLOWED_GPIOS  ]
+        if commands:
+            self.commands = commands
+        else:
+            self.commands = self.json_load(filename)
 
+        self.BTN = []
+        self.activate()
+    
+    def activate(self):
+        #reload content if needed
+        if self.BTN != [] : self.exit()
+        self.BTN = [ Button ( int(b), self.commands[b]) for b in self.commands if int(b) in self._ALLOWED_GPIOS  ]
         logging.info("Listening for buttons: %s. Press CTRL+C to exit." % ",".join([ str(B.channel) for B in self.BTN ]))
         
     def exit(self):
         GPIO.cleanup() # cleanup all GPIO
         for B in self.BTN: 
             B.stop()
+            
+    def json_create(self, content = {}, filename="/etc/gpio.conf"):
+        '''
+        Shows how to create a json file that can be used to specify GPIOs and associated commands
+        The top level values in the dictionary specify the GPIO number (BOARD format) and for each GPIO
+        one can specify actions to take after a quick click (0) or after a pression lasting x seconds.
+
+        The ethoscope PCB is connected on PINs 13,26 (BCM) or 33, 37 (BOARD) 
+        https://www.notion.so/giorgiogilestro/GPIO-PCB-9665d6c670e34f5eab229cf2d18c569d
+        '''
+        
+        if content == {}:
+            content = { 33: {0: "systemctl restart ethoscope_device", 3: "reboot"},
+                       37: {0: "ethoclient stop", 3 : "poweroff" } }
+                       
+        with open(filename, 'w') as fp:
+            json.dump(content, fp)
+            
+        return content
+            
+    def json_load(self, filename="/etc/gpio.conf"):
+        '''
+        '''
+        if os.path.exists(filename):
+            with open(filename, 'r') as fp:
+                content = json.load(fp)
+        else:
+            content = self.json_create()
+            logging.error("A valid JSON file specifying the buttons to be used was not passed as an argument. Creating a default conf file in /etc/gpio.conf")
+        
+        return content            
 
 class Button(threading.Thread):
     def __init__(self, channel, commands={ '0' : '', '5' : ''} ):
@@ -97,39 +141,11 @@ class Button(threading.Thread):
                     self._pressed = True
                     self.last_pressed = time.time()
 
-def json_create(content = {}, filename="/etc/gpio.conf"):
-    '''
-    Shows how to create a json file that can be used to specify GPIOs and associated commands
-    The top level values in the dictionary specify the GPIO number (BOARD format) and for each GPIO
-    one can specify actions to take after a quick click (0) or after a pression lasting x seconds.
-    '''
-    
-    if content == {}:
-        content = { 33: {0: "systemctl restart ethoscope_device", 3: "reboot"},
-                   37: {0: "ethoclient stop", 3 : "poweroff" } }
-                   
-    with open(filename, 'w') as fp:
-        json.dump(content, fp)
-        
-    return content
-        
-def json_load(filename="/etc/gpio.conf"):
-    '''
-    '''
-    with open(filename, 'r') as fp:
-        content = json.load(fp)
-        
-    return content
-
 if __name__ == '__main__':
-
-    #The ethoscope PCB is connected on PINs 13,26 (BCM) or 33, 37 (BOARD) https://www.notion.so/giorgiogilestro/GPIO-PCB-9665d6c670e34f5eab229cf2d18c569d
-
-    DEFAULT_JSON_FILE = "/etc/gpio.conf"
 
     parser = OptionParser()
     parser.add_option("-D", "--debug", dest="debug", default=True, help="Shows all logging messages", action="store_true")
-    parser.add_option("-C", "--jsonfile", dest="commands", help="A JSON file describing which command to associate to each button", default=DEFAULT_JSON_FILE)
+    parser.add_option("-C", "--jsonfile", dest="jsonfile", help="A JSON file describing which command to associate to each button", default=DEFAULT_JSON_FILE)
     parser.add_option("-S", "--jsonstring", dest="jsonstring", help="A JSON string describing which command to associate to each button")
     parser.add_option("-B", "--blink", dest="blink", help="Blink specified GPIO. Meant to be used as an example only")
 
@@ -153,21 +169,14 @@ if __name__ == '__main__':
 
     if option_dict["jsonstring"]:
         commands = ast.literal_eval(ast.literal_eval(json.dumps(option_dict["jsonstring"])))
+        BTNs = GPIOButtons( commands = commands )
     
+    elif option_dict["jsonfile"]:
+        BTNs = GPIOButtons( filename = option_dict["jsonfile"] )
+
     else:
-        try:
-            commands = json_load(option_dict["commands"])
-        except:
-            if not os.path.exists(DEFAULT_JSON_FILE):
-                logging.error("A valid JSON file specifying the buttons to be used was not passed as an argument. Creating a default conf file in /etc/gpio.conf")
-                try:
-                    commands = json_create()
-                except:
-                    os.sys.exit("A valid JSON configuration file could not be created in %" % DEFAULT_JSON_FILE)
-            else:
-                os.sys.exit("Error loading JSON from the provided file: %" % option_dict["commands"])
-                
-    BTNs = GPIOButtons( commands )
+        BTNs = GPIOButtons()
+
     try:
         pass
     except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
