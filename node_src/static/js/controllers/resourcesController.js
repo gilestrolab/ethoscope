70ba6e63acd7ecbaccc4c7db23a0f1f68463c534
Giorgio Gilestro <giorgio@gilest.ro>
2022-10-09 11:45:13 +0100

    Changed the http port for the news and resources server from 8001 to 8088. 8001 is reserved for UDP and blocked by many firewalls.

--- node_src/static/js/controllers/resourcesController.js
+++ node_src/static/js/controllers/resourcesController.js
@@ -1,8 +1,8 @@
 (function(){
     var resourcesController = function($scope, $http, $timeout, $routeParams, $window){
         
-        var resourcesURL = "https://lab.gilest.ro:8001/resources";
-        var newsURL = "https://lab.gilest.ro:8001/news";
+        var resourcesURL = "https://lab.gilest.ro:8088/resources";
+        var newsURL = "https://lab.gilest.ro:8088/news";
         $scope.resources = {};
         $scope.notifications = {};
         $scope.hasAccess = false;
