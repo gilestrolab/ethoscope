f8a0bad9f1cc192fc74e40c7fd5793e570eae6c1
Giorgio Gilestro <giorgio@gilest.ro>
2024-06-19 13:04:42 +0100

    Add a check-all-ethoscopes box to the updater interface

--- scripts/ethoscope_updater/static/js/script.js
+++ scripts/ethoscope_updater/static/js/script.js
@@ -1,6 +1,6 @@
-(function(){
+(function() {
     var app = angular.module('updater', ['ngRoute']);
-    app.filter("toArray", function(){
+    app.filter("toArray", function() {
         return function(obj) {
             var result = [];
             angular.forEach(obj, function(val, key) {
@@ -14,34 +14,35 @@
     app.controller('mainController', function($scope, $window, $http, $interval, $timeout) {
         $scope.system = {};
         $scope.system.isUpdated = false;
-        $scope.node ={};
-        $scope.devices={};
+        $scope.node = {};
+        $scope.devices = {};
         $scope.devices_to_update_selected = [];
         $scope.selected_devices = [];
-        $scope.modal={title:'title',
-                      info:'Some info',
-                      action_text:'action_text',
-                      action:'action',
-                     };
-        
-        $scope.branch_to_switch= null;
+        $scope.modal = {
+            title: 'title',
+            info: 'Some info',
+            action_text: 'action_text',
+            action: 'action',
+        };
+
+        $scope.branch_to_switch = null;
         $scope.system.modal_error = "";
-        $scope.spinner= new Spinner(opts).spin();
+        $scope.spinner = new Spinner(opts).spin();
         var loadingContainer = document.getElementById('loading_devices');
         loadingContainer.appendChild($scope.spinner.el);
         $scope.spinner_text = 'Fetching, please wait...';
 
-        $scope.spinner_scan= new Spinner(opts).spin();
+        $scope.spinner_scan = new Spinner(opts).spin();
         var loadingContainer_scan = document.getElementById('scanning_devices');
         loadingContainer_scan.appendChild($scope.spinner_scan.el);
 
 
-        $http.get('/bare/update').success(function(data){
+        $http.get('/bare/update').success(function(data) {
             console.log(data);
-            if ('error' in data){
-                $scope.system.error= data.error;
+            if ('error' in data) {
+                $scope.system.error = data.error;
                 console.log($scope.system.isUpdated);
-            }else{
+            } else {
                 $scope.system.isUpdated = true;
                 $scope.system.status = data;
                 console.log($scope.system.isUpdated);
@@ -51,7 +52,7 @@
             $scope.spinner_text = null;
 
         });
-        $http.get('/devices').success(function(data){
+        $http.get('/devices').success(function(data) {
             check_error(data);
             $scope.devices = data;
 
@@ -61,65 +62,81 @@
 
             console.log($scope.devices);
         });
-        $http.get('/device/check_update/node').success(function(data){
-           check_error(data);
+        $http.get('/device/check_update/node').success(function(data) {
+            check_error(data);
             $scope.node.check_update = data;
         });
-        $http.get('/device/active_branch/node').success(function(data){
+        $http.get('/device/active_branch/node').success(function(data) {
             check_error(data);
             $scope.node.active_branch = data.active_branch;
         });
         //aADD A call to node/info
-        $http.get('/node_info').success(function(data){
+        $http.get('/node_info').success(function(data) {
             check_error(data);
             $scope.node.ip = data.ip;
             $scope.node.status = data.status;
-            $scope.node.id= data.id;
+            $scope.node.id = data.id;
         });
-        
+
+        $scope.toggleAll = function(selectAll, devices) {
+            angular.forEach(devices, function(device) {
+                device.selected = selectAll;
+            });
+        };
+
+        $scope.checkAllSelected = function() {
+            var allSelected = true;
+            angular.forEach($scope.devices, function(device) {
+                if (!device.selected) {
+                    allSelected = false;
+                }
+            });
+            $scope.selectAll = allSelected;
+        };
+
         //modal controller
-        
-        $scope.activate_modal = function(devices, action){
+
+        $scope.activate_modal = function(devices, action) {
             spin("start");
             // error == false => at least one device  is NOT {stopped, NA, Software broken}
             error = check_devices_state(devices, ["stopped", "NA", "Software broken"]);
-            if(!error){
-		        switch(action){
-		            case 'update':
-		                $scope.modal={
-		                    title: 'Update devices',
-		                    info:'This devices are going to be updated. Do not disconnect them.',
-		                    action_text:'Update',
-		                    action:'update',
-		                }
-		                break;
-		            case 'restart':
-		                $scope.modal={
-		                    title: 'Restart devices',
-		                    info:'The following devices are going to be restarted.',
-		                    action_text:'Restart',
-		                    action:'restart',
-		                }
-		                break;
-		            case 'swBranch':
-		                $scope.modal={
-		                    title: 'Switch Branch devices',
-		                    info:'Select branch to switch selected devices:',
-		                    action_text:'Switch Branch',
-		                    action:'swBranch',
-		                }
-		                break;
-		        }
+            if (!error) {
+                switch (action) {
+                    case 'update':
+                        $scope.modal = {
+                            title: 'Update devices',
+                            info: 'This devices are going to be updated. Do not disconnect them.',
+                            action_text: 'Update',
+                            action: 'update',
+                        }
+                        break;
+                    case 'restart':
+                        $scope.modal = {
+                            title: 'Restart devices',
+                            info: 'The following devices are going to be restarted.',
+                            action_text: 'Restart',
+                            action: 'restart',
+                        }
+                        break;
+                    case 'swBranch':
+                        $scope.modal = {
+                            title: 'Switch Branch devices',
+                            info: 'Select branch to switch selected devices:',
+                            action_text: 'Switch Branch',
+                            action: 'swBranch',
+                        }
+                        break;
+                }
 
             }
             $("#Modal").modal('show');
             spin("stop");
         }
-        
-        $scope.modal_action = function(devices,action){
+
+        $scope.modal_action = function(devices, action) {
             $("#Modal").modal('hide');
             spin("start");
-            switch(action){
+            switch (action) {
                 case 'update':
                     url = '/group/update';
                     break;
@@ -127,69 +144,73 @@
                     url = '/group/restart';
                     break;
                 case 'swBranch':
-                    for (device in devices){
-                    devices[device]['new_branch'] = $scope.modal.branch_to_switch;
+                    for (device in devices) {
+                        devices[device]['new_branch'] = $scope.modal.branch_to_switch;
                     }
                     url = '/group/swBranch';
-                    
+
                     break;
             }
-            data = {"devices":devices};
+            data = {
+                "devices": devices
+            };
             $http.post(url, data = data)
-                    .success(function(data){
-                        var error = check_error(data);
-                        $scope.update_result= data;
-                        spin("stop");
-                        if (!error){
-                            $window.location.reload();
-                        }
-            });
+                .success(function(data) {
+                    var error = check_error(data);
+                    $scope.update_result = data;
+                    spin("stop");
+                    if (!error) {
+                        $window.location.reload();
+                    }
+                });
         }
-        
-        
+
+
         //HELPERS
-        $scope.secToDate = function(secs){
-            d = new Date(secs*1000);
+        $scope.secToDate = function(secs) {
+            d = new Date(secs * 1000);
             return d.toString();
         };
-        
-        spin = function(action){
-        if (action=="start"){
-             $scope.spinner= new Spinner(opts).spin();
-            var loadingContainer = document.getElementById('loading_devices');
-            loadingContainer.appendChild($scope.spinner.el);
-        }else if (action=="stop"){
-             $scope.spinner.stop();
-             $scope.spinner = false;
-        }
+
+        spin = function(action) {
+            if (action == "start") {
+                $scope.spinner = new Spinner(opts).spin();
+                var loadingContainer = document.getElementById('loading_devices');
+                loadingContainer.appendChild($scope.spinner.el);
+            } else if (action == "stop") {
+                $scope.spinner.stop();
+                $scope.spinner = false;
+            }
         };
-        
-        check_devices_state = function(devices, states){
+
+        check_devices_state = function(devices, states) {
             var states_dic = {};
-            for(var i=0; i<states.length;i++){states_dic[states[i]]="";}
+            for (var i = 0; i < states.length; i++) {
+                states_dic[states[i]] = "";
+            }
 
             var error = false;
-                $scope.system.modal_error = "";
-                if(devices.length == 0){
-                    $scope.system.modal_error = "No device selected. Please tick some boxes!";
+            $scope.system.modal_error = "";
+            if (devices.length == 0) {
+                $scope.system.modal_error = "No device selected. Please tick some boxes!";
+                return true;
+            }
+
+
+            for (device in devices) {
+                if (!(devices[device]["status"] in states_dic)) {
+                    //console.log(devices[device]["status"])
+                    $scope.system.modal_error = "One or more selected devices not one of: {" + states.join(", ") + "} ( so cannot be updated, remove them from the selection)."
                     return true;
+
                 }
-
-
-                for (device in devices){
-                    if (!(devices[device]["status"] in states_dic)){
-                        //console.log(devices[device]["status"])
-                        $scope.system.modal_error="One or more selected devices not one of: {"+states.join(", ")+"} ( so cannot be updated, remove them from the selection)."
-                        return true;
-
-                    }
-                };
-                return false;
+            };
+            return false;
         };
-        
-        check_error = function(data){
-            if ('error' in data){
-                $scope.system.error= data.error;
+
+        check_error = function(data) {
+            if ('error' in data) {
+                $scope.system.error = data.error;
                 return true;
             }
             return false;
@@ -197,5 +218,4 @@
 
     });
 
-}
-)()
+})()
